a901ae846d793065383df6e9fb153547bce98604
Arkadiusz Konior <akonior@gmail.com>
2023-12-18 15:53:39 +0100

    Oracle call (#2)
    * get_account oracle call

--- test/simple.test.js
+++ test/simple.test.js
@@ -3,12 +3,12 @@ import { generate_and_verify_simple_proof } from '../src/main.js'
 
 describe('suite name', () => {
   it('success', async () => {
-    expect(await generate_and_verify_simple_proof({ x: 1, y: 2 })).toEqual(true)
+    expect(await generate_and_verify_simple_proof({x: 1, y: 2})).toEqual(true)
   })
 
   it('fail', async () => {
-    await expect(generate_and_verify_simple_proof({ x: 1, y: 1 })).rejects.toThrow(
-        'Circuit execution failed: Error: Cannot satisfy constraint',
+    await expect(generate_and_verify_simple_proof({x: 1, y: 1})).rejects.toThrow(
+      'Circuit execution failed: Error: Cannot satisfy constraint',
     );
   })
 })
