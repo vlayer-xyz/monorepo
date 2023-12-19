e46538cc06c9e6c64c629a95f6579fd5450277b6
Marek Kirejczyk <marekkirejczyk@users.noreply.github.com>
2023-12-19 09:36:11 +0100

    getAccount oracle (#3)
    * Add viem to dependencies
    
    * Add dotenv to dependencies
    
    * Introduce getAccount oracles
    
    * Introduce Account struct
    
    * Fetch proof from Ethereum node

---
+++ src/oracles.js
@@ -0,0 +1,29 @@
+import initNoirAbi, { abiEncode, abiDecode, WitnessMap, Field } from '@noir-lang/noirc_abi';
+import { createPublicClient, http } from 'viem'
+import { mainnet } from 'viem/chains'
+import { argToString } from './encode.js'
+
+async function fetchAccountWithProof(address, storageKeys = []) {
+  const client = createPublicClient({
+    chain: mainnet,
+    transport: http(process.env.ETHEREUM_JSON_RPC_API_URL)
+  })
+  return client.getProof({address, storageKeys});
+}
+
+const getAccount = async (blockNo, address) => {
+  const account = await fetchAccountWithProof(argToString(address));
+  const result = [
+    account.balance.toString(16),
+    account.codeHash,
+    account.nonce.toString(16),
+  ]
+  return Promise.resolve(result)
+}
+
+export const oracles = async (name, args) => {
+  if (name === "get_account") {
+    return await getAccount(...args);
+  }
+  return Promise.reject("Unknown oracle");
+}
