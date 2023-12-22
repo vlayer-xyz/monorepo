90cf3eaa83f4d79a2d1638277bba13c846c88802
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-12-22 16:26:09 +0100

    Verifier stub

--- contracts/script/Counter.s.sol
+++ contracts/script/Counter.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
 
-contract CounterScript is Script {
+contract EthereumHistoryVerifierScript is Script {
     function setUp() public {}
 
     function run() public {
