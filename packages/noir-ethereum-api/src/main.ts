365c4f6d139447ab61a9acbb9f41029eaebc2461
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-12-23 15:14:20 +0100

    Change source files extensions js -> ts

---
+++ packages/noir-ethereum-api/src/main.ts
@@ -0,0 +1,11 @@
+import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
+import { Noir } from '@noir-lang/noir_js';
+import noir_ethereum_history_api from '../../circuit/target/noir_ethereum_history_api.json';
+import { oracles } from './oracles';
+
+export async function generate_and_verify_simple_proof(input) {
+  const backend = new BarretenbergBackend(noir_ethereum_history_api);
+  const noir = new Noir(noir_ethereum_history_api, backend);
+  const proof = await noir.generateFinalProof(input, oracles);
+  return await noir.verifyFinalProof(proof);
+};
