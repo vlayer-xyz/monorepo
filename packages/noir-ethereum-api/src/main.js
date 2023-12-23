0796b610de90c8bb7b831751876dfc36516e9204
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-12-23 14:45:07 +0100

    Update paths

--- packages/noir-ethereum-api/src/main.js
+++ packages/noir-ethereum-api/src/main.js
@@ -1,6 +1,6 @@
 import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
 import { Noir } from '@noir-lang/noir_js';
-import noir_ethereum_history_api from './circuit/target/noir_ethereum_history_api.json';
+import noir_ethereum_history_api from '../../circuit/target/noir_ethereum_history_api.json';
 import { oracles } from './oracles';
 
 export async function generate_and_verify_simple_proof(input) {
