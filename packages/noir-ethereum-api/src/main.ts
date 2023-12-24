911796cdd2d646af9da1641186cc3c2967e17c99
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-12-24 11:16:34 +0100

    More explicit hack for CompliedCircuit type mismatch

--- packages/noir-ethereum-api/src/main.ts
+++ packages/noir-ethereum-api/src/main.ts
@@ -1,11 +1,12 @@
-import { BarretenbergBackend } from '@noir-lang/backend_barretenberg';
+import { BarretenbergBackend, CompiledCircuit } from '@noir-lang/backend_barretenberg';
 import { Noir, InputMap } from '@noir-lang/noir_js';
 import noir_ethereum_history_api from '../../../circuit/target/noir_ethereum_history_api.json';
 import { oracles } from './oracles.js';
 
 export async function generate_and_verify_simple_proof(input: InputMap) {
-  const backend = new BarretenbergBackend(noir_ethereum_history_api as any);
-  const noir = new Noir(noir_ethereum_history_api as any, backend);
+  const circuit = noir_ethereum_history_api as unknown as CompiledCircuit;
+  const backend = new BarretenbergBackend(circuit);
+  const noir = new Noir(circuit, backend);
   const proof = await noir.generateFinalProof(input, oracles);
   return await noir.verifyFinalProof(proof);
 };
