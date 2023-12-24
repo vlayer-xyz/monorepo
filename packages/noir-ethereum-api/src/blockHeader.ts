a025a9608356d5e0ac6e316ca2c411199a62f232
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-12-24 22:06:39 +0100

    encodeHeader - add support for eip1559

--- packages/noir-ethereum-api/src/blockHeader.ts
+++ packages/noir-ethereum-api/src/blockHeader.ts
@@ -1,9 +1,9 @@
-import { ReturnTypeBlockHeader, toRlp, createPublicClient, http } from 'viem';
+import { ReturnTypeBlockHeader, createPublicClient, http, keccak256, numberToHex, toRlp } from 'viem';
 import { mainnet } from 'viem/chains';
 
 
 export function encodeBlockHeader(blockHeader: ReturnTypeBlockHeader) {
-  const encodedHeader = toRlp([
+  let header = [
     blockHeader.parentHash,
     blockHeader.sha3Uncles,
     blockHeader.miner,
@@ -19,6 +19,9 @@ export function encodeBlockHeader(blockHeader: ReturnTypeBlockHeader) {
     blockHeader.extraData,
     blockHeader.mixHash,
     blockHeader.nonce
-  ]);
-  return encodedHeader;
+  ];
+  if (blockHeader.baseFeePerGas) {
+    header.push(blockHeader.baseFeePerGas);
+  }
+  return toRlp(header);
 }
--- packages/noir-ethereum-api/test/blockHeader.test.ts
+++ packages/noir-ethereum-api/test/blockHeader.test.ts
@@ -1,12 +1,13 @@
 import { describe, expect, it } from 'vitest';
-import { encodeBlockHeader, fetchBlock } from '../src/blockHeader.js';
-import {blocks} from './resources/blockHeader.json';
+import { blockToHeader, calculateBlockHash, encodeBlockHeader, fetchBlock } from '../src/blockHeader.js';
+import { blocks } from './resources/blockHeader.json';
 
 
 
-describe('blockHeader', () => {
-  it('Encode test vectors', async () => {
-    expect(encodeBlockHeader(blocks[0].header)).toBe(blocks[0].rlp);
-  });
+describe('encodeBlockHeader', () => {
+  for (let block of blocks) {
+    it(block.title, async () => {
+      expect(encodeBlockHeader(block.header)).toBe(block.rlp);
+    });
+  }
 });
-
