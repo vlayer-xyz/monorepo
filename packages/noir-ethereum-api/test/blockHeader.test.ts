ca1ad8d8ae57c80145a62899f49abd2bb7cd5798
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-12-25 12:54:45 +0100

    Fix import

--- packages/noir-ethereum-api/test/blockHeader.test.ts
+++ packages/noir-ethereum-api/test/blockHeader.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { BlockHeader, calculateBlockHeaderHash, encodeBlockHeader } from '../src/blockHeader.js';
+import { BlockHeader, calculateBlockHeaderHash, encodeBlockHeader } from '../src/ethereum/blockHeader.js';
 import { blocks } from './resources/blockHeader.json';
 
 
