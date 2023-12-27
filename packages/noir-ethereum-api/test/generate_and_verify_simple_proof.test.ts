de71e9e0ab4782dfbd8d9b183afb5bc612774355
Arkadiusz Konior <akonior@gmail.com>
2023-12-27 16:03:41 +0100

    Rename Account to AccountWithProof; Provide invalid proof for magic value account address

--- packages/noir-ethereum-api/test/generate_and_verify_simple_proof.test.ts
+++ packages/noir-ethereum-api/test/generate_and_verify_simple_proof.test.ts
@@ -13,7 +13,7 @@ describe('generate_and_verify_simple_proof', () => {
 
   it('proof fails', async () => {
     await expect(generate_and_verify_simple_proof({ block_no: 18800000, address: "0xdafea492d9c6733ae3d56b7ed1adb60692c98bc5" }, oraclesStub)).rejects.toThrow(
-      'Circuit execution failed: Error: Cannot satisfy constraint',
+      'Circuit execution failed: Error: Failed to solve brillig function, reason: explicit trap hit in brillig',
     );
   })
 }, {
