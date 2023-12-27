6feb734a3c88d0c9131bcb89710c3a3c86e81e8f
Arkadiusz Konior <akonior@gmail.com>
2023-12-27 18:00:53 +0100

    Verify merkle proof (#10)
    * return account proof from oracle call
    * increase timeout
    * Add oracle hardcoded proof
    * Add oracles stub file with test data (stateRoot, key, value, proof)
    * Use oracles stub in test
    * Rename Account to AccountWithProof; Provide invalid proof for magic value account address

--- packages/noir-ethereum-api/src/noir/oracles.ts
+++ packages/noir-ethereum-api/src/noir/oracles.ts
@@ -1,15 +1,19 @@
 import { PublicClient, Address } from 'viem';
+import { ForeignCallOutput } from '@noir-lang/noir_js';
 import { assert } from '../assert.js';
 import { decodeHexAddress } from '../noir/encode.js';
 import { createDefaultClient } from '../ethereum/client.js';
 import { encodeField } from './encode.js';
 
-const getAccountOracle = async (client: PublicClient, args: string[][]) => {
+export type Oracles = (name: string, args: string[][]) => Promise<ForeignCallOutput[]>;
+
+const getAccountOracle = async (client: PublicClient, args: string[][]): Promise<ForeignCallOutput[]> => {
   assert(args.length == 2, "get_account requires 2 arguments");
   assert(args[0].length == 1, "get_account first argument must be a block number");
   assert(args[1].length == 42, "get_account second argument must be an address");
   const address = decodeHexAddress(args[1]);
   const account = await client.getProof({address, storageKeys: []});
+
   return [
     encodeField(account.balance),
     account.codeHash,
@@ -17,10 +21,9 @@ const getAccountOracle = async (client: PublicClient, args: string[][]) => {
   ];
 }
 
-export const oracles = async (name: string, args: string[][]) => {
+export const oracles: Oracles = async (name: string, args: string[][]): Promise<ForeignCallOutput[]> => {
   if (name === "get_account") {
     return await getAccountOracle(createDefaultClient(), args);
   }
   return Promise.reject("Unknown oracle");
 }
-
--- packages/noir-ethereum-api/test/generate_and_verify_simple_proof.test.ts
+++ packages/noir-ethereum-api/test/generate_and_verify_simple_proof.test.ts
@@ -1,18 +1,21 @@
 import { describe, expect, it } from 'vitest'
 import { generate_and_verify_simple_proof } from '../src/main.js'
 import dotenv from 'dotenv';
+import { oraclesStub } from './oraclesStub.js';
 
 dotenv.config();
 
 
 describe('generate_and_verify_simple_proof', () => {
   it('proof successes', async () => {
-    expect(await generate_and_verify_simple_proof({ block_no: 18800000, address: "0x4200000000000000000000000000000000000016" })).toEqual(true)
+    expect(await generate_and_verify_simple_proof({ block_no: 18800000, address: "0x4200000000000000000000000000000000000016" }, oraclesStub)).toEqual(true)
   })
 
   it('proof fails', async () => {
-    await expect(generate_and_verify_simple_proof({ block_no: 18800000, address: "0xdafea492d9c6733ae3d56b7ed1adb60692c98bc5" })).rejects.toThrow(
-      'Circuit execution failed: Error: Cannot satisfy constraint',
+    await expect(generate_and_verify_simple_proof({ block_no: 18800000, address: "0xdafea492d9c6733ae3d56b7ed1adb60692c98bc5" }, oraclesStub)).rejects.toThrow(
+      'Circuit execution failed: Error: Failed to solve brillig function, reason: explicit trap hit in brillig',
     );
   })
+}, {
+  timeout: 20000
 })
