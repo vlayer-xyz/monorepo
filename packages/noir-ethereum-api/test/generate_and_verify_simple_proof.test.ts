892b58b52e8bfdd2ad05ff635f1d383c5c4119fa
Arkadiusz Konior <akonior@gmail.com>
2023-12-28 12:59:59 +0100

    extract common test params

--- packages/noir-ethereum-api/test/generate_and_verify_simple_proof.test.ts
+++ packages/noir-ethereum-api/test/generate_and_verify_simple_proof.test.ts
@@ -3,25 +3,24 @@ import { generate_and_verify_simple_proof } from '../src/main.js'
 import { loadAccountWithProof, stubOracles } from './oraclesStub.js';
 import { AccountWithProof, Oracles, serializeAccountWithProof } from "../src/noir/oracles.js";
 
+const defaultTestCircuitInputParams = {
+  block_no: 0,
+  address: "0x0000000000000000000000000000000000000000"
+};
+
 describe('generate_and_verify_simple_proof', () => {
 
   it('proof successes', async () => {
     let accountWithProof: AccountWithProof = loadAccountWithProof('accountWithProof.json');
     const oracles: Oracles = stubOracles({ 'get_account': serializeAccountWithProof(accountWithProof) })
-    expect(await generate_and_verify_simple_proof({
-      block_no: 0,
-      address: "0x0000000000000000000000000000000000000000"
-    }, oracles)).toEqual(true)
+    expect(await generate_and_verify_simple_proof(defaultTestCircuitInputParams, oracles)).toEqual(true)
   })
 
   it('proof fails: invalid storage proof', async () => {
     let accountWithProof: AccountWithProof = loadAccountWithProof('accountWithProof.json');
     accountWithProof.proof[0] += 1
     const oracles: Oracles = stubOracles({ 'get_account': serializeAccountWithProof(accountWithProof) })
-    expect(generate_and_verify_simple_proof({
-      block_no: 0,
-      address: "0x0000000000000000000000000000000000000000"
-    }, oracles)).rejects.toThrow(
+    expect(generate_and_verify_simple_proof(defaultTestCircuitInputParams, oracles)).rejects.toThrow(
       'Circuit execution failed: Error: Failed to solve brillig function, reason: explicit trap hit in brillig',
     );
   })
