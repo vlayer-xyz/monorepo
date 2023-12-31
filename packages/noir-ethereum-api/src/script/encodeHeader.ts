3dd3d4e305214167c7524f0b31eb0f9f14c43376
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2023-12-31 20:02:43 +0100

    getHeader - check stateRoot

--- packages/noir-ethereum-api/src/script/encodeHeader.ts
+++ packages/noir-ethereum-api/src/script/encodeHeader.ts
@@ -1,15 +1,35 @@
 import { keccak256, hexToRlp, hexToBytes } from 'viem';
-import { encodeHexToBytes32 } from '../noir/encode.js';
+import { encodeHexString, encodeHexToBytes32 } from '../noir/encode.js';
 import { blockHeaders } from '../../test/fixtures/blockHeader.json';
 import { BlockHeader, encodeBlockHeader } from '../ethereum/blockHeader.js';
 import { inspect } from 'util';
 
-const hex = encodeBlockHeader(blockHeaders[0].header as BlockHeader)
+const MAX_HEADER_RLP_SIZE = 708;
 
+function formatUint8Array(array: Uint8Array | number[]) {
+    const core = array.map((item) => item).join(', ')
+    return `[${core}]`;
+}
+
+function padRightUint8Array(array: Uint8Array, n: number) {
+    const result = new Uint8Array(n);
+    for (let i = 0; i < n; i++) {
+        result[i] = i < array.length ? array[i] : 0;
+    }
+    return result;
+}
+
+const block = blockHeaders[1].header;
+const hex = encodeBlockHeader(block as BlockHeader)
 const bytes = hexToBytes(hex);
-console.log(`let input: [u8; ${bytes.length}] =`, inspect(bytes, { maxArrayLength: null }));
 
-const keccak = keccak256(bytes)
-const output = encodeHexToBytes32(keccak);
-console.log(`let output:[u8; ${output.length}] =`, output);
+console.log('let blockPartial = BlockHeaderPartial {');
+console.log(`   stateRoot: ${formatUint8Array(encodeHexString(block.stateRoot))},`);
+console.log(`   transactionsRoot: ${formatUint8Array(encodeHexString(block.transactionsRoot))},`);
+console.log(`   receiptsRoot: ${formatUint8Array(encodeHexString(block.receiptsRoot))},`);
+console.log(`   number: ${parseInt(block.number, 16)},`);
+console.log(`   encoded_len: ${bytes.length},`);
+console.log(`   encoded: ${formatUint8Array(padRightUint8Array(bytes, MAX_HEADER_RLP_SIZE))}\n\};`);
 
+const keccak = encodeHexToBytes32(keccak256(bytes));
+console.log(`let keccak: [u8; ${keccak.length}] = ${formatUint8Array(keccak)};`);
