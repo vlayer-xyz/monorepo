6236487ac1748de0b4e1479fe1a5a79e301be9f9
Marek Kirejczyk <marekkirejczyk@users.noreply.github.com>
2024-01-02 10:44:32 +0100

    Encode header tooling (#23)
    * Introduce encode header script
    
    * Introduce toHexString
    
    * Fix error in hash calculation
    
    * Change address type from string to u8 array

--- packages/noir-ethereum-api/src/noir/encode.ts
+++ packages/noir-ethereum-api/src/noir/encode.ts
@@ -20,13 +20,30 @@ export function encodeField(arg: number | bigint) {
 }
 
 export function encodeBytes32(value: bigint) {
+  return encodeBytes(value, 32);
+}
+
+export function encodeAddress(value: bigint) {
+  return encodeBytes(value, 20);
+}
+
+export function encodeBytes(value: bigint, length: number) {
   assert(value >= 0n, 'Invalid Bytes32: Negative');
-  assert(value < (1n << 256n), 'Invalid Bytes32: Overflow');
-  const hexValue = value.toString(16).padStart(64, '0');
+  assert(value < (1n << (8n * BigInt(length))), 'Invalid Bytes32: Overflow');
+  const hexValue = value.toString(16).padStart(length * 2, '0');
   const chunks = [];
   for (let i = 0; i < hexValue.length; i += 2) {
       const chunk = hexValue.substring(i, i + 2);
       chunks.push(`0x${chunk[0] == '0' ? chunk[1] : chunk}`);
+  }
+  return chunks;
+}
+
+export function encodeHexToBytes32(hex: string) {
+  const chunks = [];
+  for (let i = 2; i < hex.length; i += 2) {
+    const chunk = hex.substring(i, i + 2);
+    chunks.push(parseInt(chunk, 16));
   }
   return chunks;
 }
--- packages/noir-ethereum-api/test/e2e.test.ts
+++ packages/noir-ethereum-api/test/e2e.test.ts
@@ -1,15 +1,15 @@
 import { beforeEach, describe, expect, it } from 'vitest';
 import { createDefaultClient } from '../src/ethereum/client.js';
 import { generate_and_verify_simple_proof } from '../src/main.js';
-import { AccountWithProof, serializeAccountWithProof } from "../src/noir/oracles/accountOracles.js";
+import { encodeBytes32, encodeAddress } from '../src/noir/encode.js';
+import { serializeAccountWithProof } from "../src/noir/oracles/accountOracles.js";
 import { Oracle, Oracles, createOracles } from "../src/noir/oracles/oracles.js";
-import { expectCircuitFail } from './helpers.js';
-import { encodeBytes32 } from '../src/noir/encode.js';
 import accountWithProof from './fixtures/accountWithProof.json';
+import { expectCircuitFail } from './helpers.js';
 
 const defaultTestCircuitInputParams = {
   block_no: 0,
-  address: "0x0000000000000000000000000000000000000000"
+  address: encodeAddress(0n),
 };
 
 describe('e2e', () => {
@@ -20,7 +20,7 @@ describe('e2e', () => {
   beforeEach(async () => {
     get_account = async () => serializeAccountWithProof(accountWithProof);
     get_header = async () => [encodeBytes32(0n)]
-    oracles = createOracles(createDefaultClient())({get_account, get_header});
+    oracles = createOracles(createDefaultClient())({ get_account, get_header });
   });
 
   it('proof successes', async () => {
