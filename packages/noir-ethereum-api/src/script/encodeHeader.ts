f3f7231179790e76f69987bad105650ba4a7dfcd
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2024-01-03 17:12:17 +0100

    Refactor encode-header script

--- packages/noir-ethereum-api/src/script/encodeHeader.ts
+++ packages/noir-ethereum-api/src/script/encodeHeader.ts
@@ -1,18 +1,13 @@
 import { blockHeaders } from '../../test/fixtures/blockHeader.json';
-import { padArrayRight } from '../arrays.js';
-import { BlockHeader, encodeBlockHeader } from '../ethereum/blockHeader.js';
-import { encodeHex } from '../noir/encode.js';
-import { MAX_HEADER_RLP_SIZE } from '../noir/oracles/headerOracle.js';
+import { encodeBlockHeaderPartial } from '../noir/oracles/headerOracle.js';
 
-const block = blockHeaders[1].header;
-const hex = encodeBlockHeader(block as BlockHeader)
-const bytes = encodeHex(hex);
-const encoded = padArrayRight(bytes, MAX_HEADER_RLP_SIZE, "0x");
+const partial = encodeBlockHeaderPartial(blockHeaders[1].header);
 
-console.log('stateRoot: ', encodeHex(block.stateRoot));
-console.log('transactionsRoot: ', encodeHex(block.transactionsRoot));
-console.log('receiptsRoot: ', encodeHex(block.receiptsRoot));
-console.log('number: ', parseInt(block.number, 16));
-console.log('encoded_len: ', encoded.length);
-console.log('encoded: ');
-console.dir(encoded, { 'maxArrayLength': null });
+console.log('stateRoot:', partial[0]);
+console.log('transactionsRoot:', partial[1]);
+console.log('receiptsRoot:', partial[2]);
+console.log('number:', partial[3]);
+console.log('encoded_len:', partial[4]);
+console.log('encoded:');
+console.dir(partial[5], { 'maxArrayLength': null });
+
