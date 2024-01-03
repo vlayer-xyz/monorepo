07cbc8f5d42c9a518af932829435348565c0a2bb
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2024-01-03 17:19:28 +0100

    cast BlockHeader in script

--- packages/noir-ethereum-api/src/script/encodeHeader.ts
+++ packages/noir-ethereum-api/src/script/encodeHeader.ts
@@ -1,7 +1,8 @@
 import { blockHeaders } from '../../test/fixtures/blockHeader.json';
+import { BlockHeader } from '../ethereum/blockHeader.js';
 import { encodeBlockHeaderPartial } from '../noir/oracles/headerOracle.js';
 
-const partial = encodeBlockHeaderPartial(blockHeaders[1].header);
+const partial = encodeBlockHeaderPartial(blockHeaders[1].header as BlockHeader);
 
 console.log('stateRoot:', partial[0]);
 console.log('transactionsRoot:', partial[1]);
