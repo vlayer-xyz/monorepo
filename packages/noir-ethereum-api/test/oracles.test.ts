81ea9c03898650e6a00a29c5eeec91c14bd50a8c
Kirejczyk Marek <marek.kirejczyk@gmail.com>
2024-01-07 17:13:42 +0100

    Fix typecheck

--- packages/noir-ethereum-api/test/oracles.test.ts
+++ packages/noir-ethereum-api/test/oracles.test.ts
@@ -1,14 +1,15 @@
 import { createOracles } from '../src/noir/oracles/oracles.js';
 import { describe, expect, it } from 'vitest';
+import { type PublicClient } from 'viem';
 
 describe('importOracles', () => {
   it('success', async() => {
-    const oracle = createOracles({})({ stub: async() => await Promise.resolve(['7']) });
+    const oracle = createOracles({} as PublicClient)({ stub: async() => await Promise.resolve(['7']) });
     expect(await oracle('stub', [])).toStrictEqual(['7']);
   });
 
   it('throws when non-existing oracle', async() => {
-    const oracle = createOracles({})({});
+    const oracle = createOracles({} as PublicClient)({});
     await expect(oracle('non-existing', [])).rejects.toThrow();
   });
 });
