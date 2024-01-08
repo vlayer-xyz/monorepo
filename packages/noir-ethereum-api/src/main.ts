5c0fcc15b4ad1ba9b6d5bada7667d1a2f44e5dcd
Leonid Logvinov <logvinov.leon@gmail.com>
2024-01-08 16:00:29 +0100

    Fix linting issues

--- packages/noir-ethereum-api/src/main.ts
+++ packages/noir-ethereum-api/src/main.ts
@@ -1,21 +1,21 @@
 import {
   BarretenbergBackend,
-  type CompiledCircuit,
-} from "@noir-lang/backend_barretenberg";
-import { type InputMap, Noir } from "@noir-lang/noir_js";
-// @ts-ignore
-import noir_ethereum_history_api from "../../../circuit/target/noir_ethereum_history_api.json";
-import { type Oracles, defaultOracles } from "./noir/oracles/oracles.js";
+  type CompiledCircuit
+} from '@noir-lang/backend_barretenberg';
+import { type InputMap, Noir } from '@noir-lang/noir_js';
+// @ts-expect-error The circuit is not yet compiled during the typecheck phase
+import noir_ethereum_history_api from '../../../circuit/target/noir_ethereum_history_api.json';
+import { type Oracles, defaultOracles } from './noir/oracles/oracles.js';
 
 export interface MainInputs extends InputMap {
-  block_no: number;
-  address: string[];
-  state_root: string[];
+  block_no: number
+  address: string[]
+  state_root: string[]
 }
 
 export async function generateAndVerifyStorageProof(
   input: MainInputs,
-  oracles: Oracles = defaultOracles,
+  oracles: Oracles = defaultOracles
 ): Promise<boolean> {
   const circuit = noir_ethereum_history_api as unknown as CompiledCircuit;
   const backend = new BarretenbergBackend(circuit);
