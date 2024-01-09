3b60fe4369686ad9a40b482eac54ab59392fb916
Arkadiusz Konior <akonior@gmail.com>
2024-01-09 12:37:38 +0100

    getHeaderOracle function

--- packages/noir-ethereum-api/src/noir/oracles/headerOracle.ts
+++ packages/noir-ethereum-api/src/noir/oracles/headerOracle.ts
@@ -2,15 +2,21 @@ import { type ForeignCallOutput } from '@noir-lang/noir_js';
 import { type BlockHeader, blockToHeader, headerToRlp } from '../../ethereum/blockHeader.js';
 import { decodeField, encodeField, encodeHex } from '../encode.js';
 import { padArray } from '../../arrays.js';
-import { GetBlockReturnType, hexToBytes, isHex, keccak256, PublicClient } from 'viem';
+import { type GetBlockReturnType, hexToBytes, isHex, keccak256, type PublicClient } from 'viem';
 import { assert } from '../../assert.js';
 
 export const MAX_HEADER_RLP_SIZE = 708;
 
-export function parseArguments(args: string[][]): bigint {
-  assert(args.length == 1, "get_header requires 1 argument");
-  assert(args[0].length == 1, "get_account first argument must be an array of length 1");
-  assert(isHex(args[0][0]), "get_account first argument must be a hex value");
+export const getHeaderOracle = async(client: PublicClient, args: string[][]): Promise<ForeignCallOutput[]> => {
+  const blockNumber: bigint = parseNoirGetHeaderArguments(args);
+  const blockHeader: BlockHeader = await getBlock(client, blockNumber);
+  return encodeBlockHeaderPartial(blockHeader);
+};
+
+export function parseNoirGetHeaderArguments(args: string[][]): bigint {
+  assert(args.length === 1, 'get_header requires 1 argument');
+  assert(args[0].length === 1, 'get_account first argument must be an array of length 1');
+  assert(isHex(args[0][0]), 'get_account first argument must be a hex value');
   return decodeField(args[0][0]);
 }
 
