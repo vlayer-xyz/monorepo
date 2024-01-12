2cd8a61f21560d032490d46fbef3affe3d345ac2
Arkadiusz Konior <akonior@gmail.com>
2024-01-12 12:39:11 +0100

    remove previous implementation

--- packages/noir-ethereum-api/src/ethereum/client.ts
+++ packages/noir-ethereum-api/src/ethereum/client.ts
@@ -1,12 +1,4 @@
-import {
-  createPublicClient,
-  GetBlockParameters,
-  GetBlockReturnType,
-  GetProofParameters,
-  GetProofReturnType,
-  http,
-  type PublicClient
-} from 'viem';
+import { createPublicClient, http, type PublicClient } from 'viem';
 import { mainnet } from 'viem/chains';
 import dotenv from 'dotenv';
 
@@ -17,37 +9,4 @@ export function createDefaultClient(): PublicClient {
     chain: mainnet,
     transport: http(process.env.ETHEREUM_JSON_RPC_API_URL)
   });
-}
-
-export interface Recordings {
-  method: string;
-  arguments: object;
-  result: object;
-}
-
-export function recordingClient(client: PublicClient): PublicClient & { getRecordings: () => Recordings[] } {
-  const records: Recordings[] = [];
-
-  return {
-    ...client,
-    getProof: async (args: GetProofParameters): Promise<GetProofReturnType> => {
-      const proof = await client.getProof(args);
-      records.push({
-        method: 'getProof',
-        arguments: args,
-        result: proof
-      });
-      return proof;
-    },
-    getBlock: async (args: GetBlockParameters): Promise<GetBlockReturnType> => {
-      const block: GetBlockReturnType = (await client.getBlock(args)) as GetBlockReturnType;
-      records.push({
-        method: 'getBlock',
-        arguments: args,
-        result: block
-      });
-      return block;
-    },
-    getRecordings: () => records
-  } as PublicClient & { getRecordings: () => Recordings[] };
 }
