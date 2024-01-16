9f27caf0afc28fd4ab80dbea0d52d3b408eded8f
Arkadiusz Konior <akonior@gmail.com>
2024-01-16 14:57:59 +0100

    add object equals util

---
+++ packages/noir-ethereum-api/src/utils/object.ts
@@ -0,0 +1,37 @@
+export function equals(obj1: any, obj2: any): boolean {
+  if (obj1 === obj2) {
+    return true;
+  }
+
+  if (obj1 == null || obj2 == null || typeof obj1 !== typeof obj2) {
+    return false;
+  }
+
+  if (Array.isArray(obj1) && Array.isArray(obj2)) {
+    if (obj1.length !== obj2.length) {
+      return false;
+    }
+    for (let i = 0; i < obj1.length; i++) {
+      if (!equals(obj1[i], obj2[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  if (typeof obj1 === 'object' && typeof obj2 === 'object') {
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+    if (keys1.length !== keys2.length) {
+      return false;
+    }
+    for (const key of keys1) {
+      if (!keys2.includes(key) || !equals(obj1[key], obj2[key])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  return obj1 === obj2;
+}
---
+++ packages/noir-ethereum-api/test/utils/object.test.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from 'vitest';
+import { equals } from '../../src/utils/object.js';
+
+describe('equals', () => {
+  it('true', () => {
+    expect(equals(1, 1)).toBe(true);
+    expect(equals([1, 2], [1, 2])).toBe(true);
+    expect(equals({ a: 1 }, { a: 1 })).toBe(true);
+    expect(equals({ a: 1, b: { c: 'a' } }, { a: 1, b: { c: 'a' } })).toBe(true);
+  });
+
+  it('false', () => {
+    expect(equals(1, 2)).toBe(false);
+    expect(equals([1, 2], [1, 3])).toBe(false);
+    expect(equals({ a: 1 }, { a: 2 })).toBe(false);
+    expect(equals({ a: 1, b: { c: 'a' } }, { a: 1, b: { c: 'b' } })).toBe(false);
+  });
+});
