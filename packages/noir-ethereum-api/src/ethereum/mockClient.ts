31b0d0ecf78a39d7dc930ee71a3c8f1553d30e22
Arkadiusz Konior <akonior@gmail.com>
2024-01-17 09:20:16 +0100

    get rid of `any` types

--- packages/noir-ethereum-api/src/ethereum/mockClient.ts
+++ packages/noir-ethereum-api/src/ethereum/mockClient.ts
@@ -7,24 +7,17 @@ import { equals } from '../utils/object.js';
 export function createMockingClient(filePath: string): PublicClient {
   const savedCalls = readObjectSync<Call[]>(filePath);
 
-  return createMock<PublicClient>((method: string, args) => {
-    const call = savedCalls.find((it) => equals(it.method, method) && equals(it.arguments, args));
+  return createMock<PublicClient>((method: string, args: object): object => {
+    const call: Call | undefined = savedCalls.find((it) => equals(it.method, method) && equals(it.arguments, args));
     assert(!!call, `call not found for: ${method}(${args})`);
     return call.result;
   });
 }
 
-export function createMock<T>(methodHandler: (method: string, args: object) => object): T {
-  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-  const handler: ProxyHandler<any> = {
-    get(target, prop: string, receiver) {
-      if (!(prop in target)) {
-        // eslint-disable-next-line  @typescript-eslint/no-explicit-any
-        target[prop] = (...args: any[]) => {
-          return methodHandler(prop, args);
-        };
-      }
-      return Reflect.get(target, prop, receiver);
+export function createMock<T extends object>(methodHandler: (method: string, args: object) => object): T {
+  const handler: ProxyHandler<T> = {
+    get(_target: T, prop: string) {
+      return (...args: object[]) => methodHandler(prop, args);
     }
   };
 
