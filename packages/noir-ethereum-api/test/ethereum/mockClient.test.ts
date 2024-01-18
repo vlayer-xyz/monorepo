86c61955ffdd220a2f01a1f414581753f2d95615
Arkadiusz Konior <akonior@gmail.com>
2024-01-18 14:57:19 +0100

    inline mock client expects; remove test from recording

--- packages/noir-ethereum-api/test/ethereum/mockClient.test.ts
+++ packages/noir-ethereum-api/test/ethereum/mockClient.test.ts
@@ -1,13 +1,19 @@
-import { describe, it } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { PublicClient } from 'viem';
 import { createMockClient } from '../../src/ethereum/mockClient.js';
-import { expectPublicClientBehaviour } from './recordingClient.test.js';
+import { GET_BLOCK_PARAMETERS, GET_PROOF_PARAMETERS } from './recordingClient.test.js';
+import { readObject } from '../../src/utils/file.js';
 
 describe('mockingClient', () => {
   it('read recorded JSON-RPC API calls from file in mocking client', async () => {
     const filePath = './test/fixtures/mockClientData.json';
     const mockingClient: PublicClient = await createMockClient(filePath);
 
-    await expectPublicClientBehaviour(mockingClient);
+    const block = await mockingClient.getBlock(GET_BLOCK_PARAMETERS);
+    const proof = await mockingClient.getProof(GET_PROOF_PARAMETERS);
+
+    const data: { result: object }[] = await readObject(filePath);
+    expect(block).toStrictEqual(data[0].result);
+    expect(proof).toStrictEqual(data[1].result);
   });
 });
--- packages/noir-ethereum-api/test/ethereum/recordingClient.test.ts
+++ packages/noir-ethereum-api/test/ethereum/recordingClient.test.ts
@@ -1,15 +1,8 @@
 import { describe, expect, it } from 'vitest';
 import { Call, createRecordingClient } from '../../src/ethereum/recordingClient.js';
-import { readObject, withTempFile, writeObject } from '../../src/utils/file.js';
+import { withTempFile, writeObject } from '../../src/utils/file.js';
 import { createMockClient } from '../../src/ethereum/mockClient.js';
-import {
-  GetBlockParameters,
-  GetBlockReturnType,
-  GetProofParameters,
-  GetProofReturnType,
-  Hex,
-  PublicClient
-} from 'viem';
+import { GetBlockParameters, GetProofParameters, Hex, PublicClient } from 'viem';
 
 export const BLOCK_NUMBER = 14194126n;
 export const ADDRESS: Hex = '0xb47e3cd837dDF8e4c57f05d70ab865de6e193bbb';
@@ -46,24 +39,14 @@ const EXPECTED_CALLS = [
   }
 ];
 
-export const expectPublicClientBehaviour = async (client: PublicClient) => {
-  const getBlock = (await client.getBlock(GET_BLOCK_PARAMETERS)) as GetBlockReturnType;
-  const getProof = (await client.getProof(GET_PROOF_PARAMETERS)) as GetProofReturnType;
-
-  expect(getBlock.number).toBe(BLOCK_NUMBER);
-  expect(getBlock.hash).toBe(BLOCK_HASH);
-
-  expect(getProof.storageHash).toBe(STORAGE_HASH);
-};
-
 describe('recordingClient', async () => {
-  const mockPublicClient: PublicClient = {
+  const publicClientMock: PublicClient = {
     getBlock: () => EXPECTED_CALLS[0].result,
     getProof: () => EXPECTED_CALLS[1].result
   } as unknown as PublicClient;
 
   it('record JSON-RPC API calls', async () => {
-    const client = createRecordingClient(mockPublicClient);
+    const client = createRecordingClient(publicClientMock);
 
     client.getBlock(GET_BLOCK_PARAMETERS);
     client.getProof(GET_PROOF_PARAMETERS);
@@ -72,22 +55,9 @@ describe('recordingClient', async () => {
     expect(calls).toMatchObject(EXPECTED_CALLS);
   });
 
-  it('save recorded JSON-RPC API calls to file', async () => {
-    await withTempFile(async (tempFilePath) => {
-      const client = createRecordingClient(mockPublicClient);
-
-      client.getBlock(GET_BLOCK_PARAMETERS);
-      client.getProof(GET_PROOF_PARAMETERS);
-      await writeObject(await client.getCalls(), tempFilePath);
-
-      const savedCalls = await readObject(tempFilePath);
-      expect(savedCalls).toMatchObject(EXPECTED_CALLS);
-    });
-  });
-
   it('record and mock', async () => {
     await withTempFile(async (tempFilePath) => {
-      const client = createRecordingClient(mockPublicClient);
+      const client = createRecordingClient(publicClientMock);
 
       client.getBlock(GET_BLOCK_PARAMETERS);
       client.getProof(GET_PROOF_PARAMETERS);
@@ -95,7 +65,13 @@ describe('recordingClient', async () => {
 
       const mockingClient: PublicClient = await createMockClient(tempFilePath);
 
-      await expectPublicClientBehaviour(mockingClient);
+      const block = await mockingClient.getBlock(GET_BLOCK_PARAMETERS);
+      const proof = await mockingClient.getProof(GET_PROOF_PARAMETERS);
+
+      expect(block.number).toBe(BLOCK_NUMBER);
+      expect(block.hash).toBe(BLOCK_HASH);
+
+      expect(proof.storageHash).toBe(STORAGE_HASH);
     });
   });
 });
