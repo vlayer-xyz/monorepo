e0747436eb4e4606babd25fedf903550ea75acf9
Leonid Logvinov <logvinov.leon@gmail.com>
2024-01-24 12:44:20 +0100

    Delete e2e test fixtures and use real data from circuit

--- packages/noir-ethereum-api/test/e2e.test.ts
+++ packages/noir-ethereum-api/test/e2e.test.ts
@@ -1,15 +1,26 @@
-import { describe, expect, it } from 'vitest';
+import { beforeAll, describe, expect, it } from 'vitest';
 import { incHexStr } from '../src/util/string.js';
 import { verifyStorageProof, readProof, readWitnessMap, readInputMap, circuit } from '../src/main.js';
 import { updateNestedField } from '../src/util/object.js';
-import { abiEncode } from '@noir-lang/noirc_abi';
+import { InputMap, WitnessMap, abiEncode } from '@noir-lang/noirc_abi';
+
+const PROOF_PATH = '../../circuit/proofs/noir_ethereum_history_api.proof';
+const INPUT_MAP_PATH = '../../circuit/Verifier.toml';
 
 describe(
   'e2e',
   () => {
+    let proof: Uint8Array;
+    let inputMap: InputMap;
+    let witnessMap: WitnessMap;
+
+    beforeAll(async () => {
+      proof = await readProof(PROOF_PATH);
+      inputMap = await readInputMap(INPUT_MAP_PATH);
+      witnessMap = await readWitnessMap(INPUT_MAP_PATH);
+    });
+
     it('proof verification successes', async () => {
-      const proof = await readProof('test/fixtures/proof/noir_ethereum_history_api.proof');
-      const witnessMap = await readWitnessMap('test/fixtures/proof/Verifier.toml');
       const proofData = {
         proof,
         publicInputs: Array.from(witnessMap.values())
@@ -18,8 +29,6 @@ describe(
     });
 
     it('proof fails: invalid proof', async () => {
-      const proof = await readProof('test/fixtures/proof/noir_ethereum_history_api.proof');
-      const inputMap = await readInputMap('test/fixtures/proof/Verifier.toml');
       updateNestedField(inputMap, ['return', 'proof', '0'], incHexStr);
       const witnessMap = abiEncode(circuit.abi, inputMap, inputMap['return']);
       const proofData = {
@@ -30,8 +39,6 @@ describe(
     });
 
     it('proof fails: invalid state root', async () => {
-      const proof = await readProof('test/fixtures/proof/noir_ethereum_history_api.proof');
-      const inputMap = await readInputMap('test/fixtures/proof/Verifier.toml');
       updateNestedField(inputMap, ['state_root', '0'], incHexStr);
       const witnessMap = abiEncode(circuit.abi, inputMap, inputMap['return']);
       const proofData = {
