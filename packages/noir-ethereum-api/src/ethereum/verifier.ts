5d3bbc5fa4ec537237c99c35a28deaa0d72388ba
Arkadiusz Konior <akonior@gmail.com>
2024-02-07 09:21:38 +0100

    improve typing

--- packages/noir-ethereum-api/src/ethereum/verifier.ts
+++ packages/noir-ethereum-api/src/ethereum/verifier.ts
@@ -1,16 +1,16 @@
 import ultraVerifier from '../../../../contracts/out/UltraVerifier.sol/UltraVerifier.json';
 import { decodeHexString } from '../noir/noir_js/encode.js';
 import { Account } from 'viem/accounts';
-import { Hex, WalletClient } from 'viem';
+import { Address, Hash, WalletClient } from 'viem';
 import { WitnessMap } from '@noir-lang/noirc_abi';
 
 export async function verifyStorageProofInSolidity(
   client: WalletClient,
   account: Account,
-  contractAddress: Hex,
+  contractAddress: Address,
   proof: Uint8Array,
   witnessMap: WitnessMap
-): Promise<Hex> {
+): Promise<Hash> {
   return await client.writeContract({
     account,
     address: contractAddress,
