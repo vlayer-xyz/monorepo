49a70f98ada2c11514cc330360e6ecda7d835291
Leonid Logvinov <logvinov.leon@gmail.com>
2024-02-07 12:49:28 +0100

    Remove unconstrained proof verification as we now have solidity e2e tests (#74)
    * Make assert_account_proof constrained
    
    * Relax test gas limit
    
    * Update proof

--- packages/noir-ethereum-api/test/e2e.test.ts
+++ packages/noir-ethereum-api/test/e2e.test.ts
@@ -14,7 +14,7 @@ import { AnvilClient, createAnvilClient } from './ethereum/anvilClient.js';
 const PROOF_PATH = '../../proofs/main.proof';
 const INPUT_MAP_PATH = '../../circuits/main/Verifier.toml';
 const ANVIL_TEST_ACCOUNT_PRIVATE_KEY = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
-const VERIFICATION_GAS_LIMIT = 475_455n;
+const VERIFICATION_GAS_LIMIT = 500_000n;
 
 describe.concurrent(
   'e2e',
