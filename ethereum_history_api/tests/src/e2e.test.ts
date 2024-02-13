6a263336c359644aa491d014958dba3f86dbbc8b
Leonid Logvinov <logvinov.leon@gmail.com>
2024-02-13 15:11:14 +0100

    Run e2e tests in a non-concurrent way

--- ethereum_history_api/tests/src/e2e.test.ts
+++ ethereum_history_api/tests/src/e2e.test.ts
@@ -14,7 +14,7 @@ const INPUT_MAP_PATH = '../circuits/lib/Verifier.toml';
 const ANVIL_TEST_ACCOUNT_PRIVATE_KEY = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
 const VERIFICATION_GAS_LIMIT = 500_000n;
 
-describe.concurrent(
+describe(
   'e2e',
   () => {
     let proof: Uint8Array;
