294614f7d465ed21b817fd34639c3b89a5bfc30f
Leonid Logvinov <logvinov.leon@gmail.com>
2024-02-14 15:44:48 +0100

    Remove unused code

--- ethereum_history_api/oracles/test/helpers.ts
+++ ethereum_history_api/oracles/test/helpers.ts
@@ -1,15 +1,4 @@
-import { expect } from 'vitest';
 import { ForeignCallOutput } from '@noir-lang/noir_js';
-
-export async function expectCircuitFail(p: Promise<boolean>): Promise<void> {
-  await expect(p).rejects.toThrow(
-    'Circuit execution failed: Error: Failed to solve brillig function, reason: explicit trap hit in brillig'
-  );
-}
-
-export type FieldsOfType<ObjectType, FieldType> = {
-  [K in keyof ObjectType]: ObjectType[K] extends FieldType ? K : never;
-}[keyof ObjectType];
 
 export interface Account {
   nonce: string;
