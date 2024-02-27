1a49b1e3811c6c6e02b7140a9623c0868e90f855
Arkadiusz Konior <akonior@gmail.com>
2024-02-27 13:33:04 +0100

    With oracle server (#97)
    * Add withOracleServer method
    
    * test close oracle server
    
    * oracle server tests
    
    * Configure port number in tests
    
    * Error handling in test
    
    * Test functions rename
    
    * Introduce expectServerUp and expectServerDown methods; Simplify withMockOracleServer signature
    
    * New line at the end od package.json

---
+++ ethereum_history_api/oracles/src/noir/oracles/server.test.ts
@@ -0,0 +1,57 @@
+import { describe, it, expect } from 'vitest';
+import { withMockOracleServer } from './server.js';
+
+const JSON_RPC_PAYLOAD = {
+  jsonrpc: '2.0',
+  method: 'get_header',
+  params: [{ Single: { inner: 'd895ce' } }],
+  id: 1
+};
+
+const GET_HEADER_POST_DATA = {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify(JSON_RPC_PAYLOAD)
+};
+
+const MOCK_ORACLE_SERVER_URL = `http://localhost:5556`;
+
+async function expectServerUp(serverUrl: string) {
+  const response = await fetch(serverUrl, GET_HEADER_POST_DATA);
+  expect(response.status).toStrictEqual(200);
+}
+
+async function expectServerDown(serverUrl: string) {
+  await expect(async () => await fetch(serverUrl, GET_HEADER_POST_DATA)).rejects.toThrow('fetch failed');
+}
+
+describe('mock oracle server', async () => {
+  it('start server', async () => {
+    await withMockOracleServer(async (serverUrl) => {
+      await expectServerUp(serverUrl);
+    });
+  });
+
+  it('return callback value', async () => {
+    const result = await withMockOracleServer(async () => 'callback return value');
+
+    expect(result).toStrictEqual('callback return value');
+  });
+
+  it('close server after callback finish', async () => {
+    await withMockOracleServer(async () => {});
+
+    await expectServerDown(MOCK_ORACLE_SERVER_URL);
+  });
+
+  it('close server when callback throws an exception', async () => {
+    expect(
+      async () =>
+        await withMockOracleServer(async () => {
+          throw new Error('error');
+        })
+    ).rejects.toThrow('error');
+
+    await expectServerDown(MOCK_ORACLE_SERVER_URL);
+  });
+});
--- ethereum_history_api/oracles/src/noir/oracles/server.ts
+++ ethereum_history_api/oracles/src/noir/oracles/server.ts
@@ -1,19 +1,37 @@
+import { PublicClient } from 'viem';
 import { buildOracleServer } from './server/app.js';
+import { createMockClient } from '../../ethereum/mockClient.js';
 
 const PORT = 5555;
+const MOCK_ORACLE_SERVER_PORT = PORT + 1;
 
-const app = buildOracleServer({
-  logger: {
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        translateTime: 'SYS:standard',
-        ignore: 'pid,hostname,req.hostname,req.remoteAddress,req.remotePort'
+export const startOracleServer = async (client: PublicClient, port: number = PORT) => {
+  const app = buildOracleServer(
+    {
+      logger: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            translateTime: 'SYS:standard',
+            ignore: 'pid,hostname,req.hostname,req.remoteAddress,req.remotePort'
+          }
+        }
       }
-    }
-  }
-});
+    },
+    client
+  );
 
-app.listen({ port: PORT }, (err) => {
-  if (err) throw err;
-});
+  await app.listen({ port });
+  return app;
+};
+
+export async function withMockOracleServer<T>(fn: (serverUrl: string) => Promise<T>): Promise<T> {
+  const mockClient = await createMockClient('./fixtures/mockClientData.json');
+  const app = await startOracleServer(mockClient, MOCK_ORACLE_SERVER_PORT);
+  const serverUrl = `http://localhost:${MOCK_ORACLE_SERVER_PORT}`;
+  try {
+    return await fn(serverUrl);
+  } finally {
+    await app.close();
+  }
+}
