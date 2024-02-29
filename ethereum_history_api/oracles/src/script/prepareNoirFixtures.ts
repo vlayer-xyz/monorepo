605ec6abd1e6786caa7b17008e9880f148b0aa35
Arkadiusz Konior <akonior@gmail.com>
2024-02-29 13:17:06 +0100

    Add USDC token Circle balance

--- ethereum_history_api/oracles/src/script/prepareNoirFixtures.ts
+++ ethereum_history_api/oracles/src/script/prepareNoirFixtures.ts
@@ -4,30 +4,45 @@ import { createHeaderFixture } from './noir_fixtures/header.js';
 import { createStateProofFixture } from './noir_fixtures/state_proof.js';
 import { createAccountFixture } from './noir_fixtures/account.js';
 
+const USDC_TOKEN_CONTRACT_ADDRESS = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
+const CIRCLE_USDC_BALANCE_STORAGE_KEY = '0x57d18af793d7300c4ba46d192ec7aa095070dde6c52c687c6d0d92fb8532b305';
+
 const FIXTURES = {
   frontier: {
     first: {
       blockNumber: 1n,
-      address: '0x40d45d9d7625d15156c932b771ca7b0527130958'
+      address: '0x40d45d9d7625d15156c932b771ca7b0527130958',
+      storageKeys: []
     }
   },
   london: {
     crypto_punks: {
       blockNumber: 14_194_126n,
-      address: '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb'
+      address: '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb',
+      storageKeys: []
     },
     vitalik_balance: {
       blockNumber: 12_965_000n,
-      address: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045'
+      address: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045',
+      storageKeys: []
     }
   },
   paris: {
-    one_inch: {
+    usdc: {
       blockNumber: 19_000_000n,
-      address: '0x1111111254EEB25477B68fb85Ed929f73A960582'
+      address: USDC_TOKEN_CONTRACT_ADDRESS,
+      storageKeys: [CIRCLE_USDC_BALANCE_STORAGE_KEY]
     }
   }
-} as { [fork: string]: { [fixtureName: string]: { blockNumber: bigint; address: `0x${string}` } } };
+} as {
+  [fork: string]: {
+    [fixtureName: string]: {
+      blockNumber: bigint;
+      address: `0x${string}`;
+      storageKeys: `0x${string}`[];
+    };
+  };
+};
 
 const OUT_DIR = '../circuits/lib/src/fixtures';
 await rm(OUT_DIR, { recursive: true, force: true });
@@ -38,12 +53,12 @@ for (const hardFork in FIXTURES) {
   const hardforkModuleFile = `${OUT_DIR}/${hardFork}.nr`;
 
   for (const fixtureName in FIXTURES[hardFork]) {
-    const { blockNumber, address } = FIXTURES[hardFork][fixtureName];
+    const { blockNumber, address, storageKeys } = FIXTURES[hardFork][fixtureName];
 
     const block = await client.getBlock({ blockNumber });
     const stateProof = await client.getProof({
       address,
-      storageKeys: [],
+      storageKeys: storageKeys,
       blockNumber
     });
 
