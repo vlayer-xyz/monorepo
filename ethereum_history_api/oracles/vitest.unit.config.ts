6d0be543386cd7acdb08acbab45c43aadde71b56
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-03-01 10:37:51 +0100

    Update vitest and coverage scripts

--- ethereum_history_api/oracles/vitest.unit.config.ts
+++ ethereum_history_api/oracles/vitest.unit.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['src/**/*.test.ts']
+    include: ['src/**/*.test.ts'],
+    coverage: {
+      include: ['src/**/*.ts'],
+      exclude: ['src/script/**/*.ts', '**/types.ts']
+    }
   }
 });
