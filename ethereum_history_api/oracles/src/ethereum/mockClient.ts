e05bb270d6c93b3e3ca76cb278344a05e4fef245
Leonid Logvinov <logvinov.leon@gmail.com>
2024-03-05 00:35:22 +0100

    Use json-bigint to stringify args

--- ethereum_history_api/oracles/src/ethereum/mockClient.ts
+++ ethereum_history_api/oracles/src/ethereum/mockClient.ts
@@ -5,6 +5,7 @@ import { readObject } from '../util/file.js';
 import { mock } from '../util/mock.js';
 import isEqual from 'lodash.isequal';
 import { identity } from '../util/function.js';
+import { stringify } from '../util/json-bigint.js';
 
 export async function createMockClient(
   filePath: string,
@@ -14,7 +15,7 @@ export async function createMockClient(
 
   return mock<PublicClient>(isEthereumApiMethod, (method: string, args: unknown): unknown => {
     const call: Call | undefined = savedCalls.find((it) => it.method === method && isEqual(it.arguments, args));
-    assert(!!call, `call not found for: ${method}(${JSON.stringify(args)})`);
+    assert(!!call, `call not found for: ${method}(${stringify(args)})`);
     return resultModifier(call).result;
   });
 }
