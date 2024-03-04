32ee7a6ca9848b3a54dd1ac29024ea0065656c06
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-03-04 14:06:39 +0100

    Change getProof test data + append newlines to jsons

--- ethereum_history_api/oracles/src/script/fetchProofs.ts
+++ ethereum_history_api/oracles/src/script/fetchProofs.ts
@@ -1,5 +1,5 @@
 import { createDefaultClient } from '../ethereum/client.js';
-import { writeFile } from 'fs/promises';
+import { writeFile, appendFile } from 'fs/promises';
 import { stringify } from '../util/json-bigint.js';
 import { CIRCLE_USDC_BALANCE_STORAGE_KEY } from './prepareNoirFixtures.js';
 import { type GetProofParameters, type GetProofReturnType } from 'viem';
@@ -9,29 +9,28 @@ const filePathStorage = './fixtures/eth_getProofWithStorage_response.json';
 
 const getProofParams: GetProofParameters = {
   address: '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb',
-  storageKeys: [CIRCLE_USDC_BALANCE_STORAGE_KEY],
+  storageKeys: [],
   blockNumber: 14194126n
 };
 
+const getProofWithStorageParams: GetProofParameters = {
+  address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  storageKeys: [CIRCLE_USDC_BALANCE_STORAGE_KEY],
+  blockNumber: 19_000_000n
+};
+
 const client = createDefaultClient();
+
 console.log('Downloading proofs...');
-
-const proof: GetProofReturnType = await client.getProof({
-  address: getProofParams.address,
-  storageKeys: [],
-  blockNumber: getProofParams.blockNumber
-});
-
-const proofWithStorage: GetProofReturnType = await client.getProof({
-  address: getProofParams.address,
-  storageKeys: getProofParams.storageKeys,
-  blockNumber: getProofParams.blockNumber
-});
+const proof: GetProofReturnType = await client.getProof(getProofParams);
+const proofWithStorage: GetProofReturnType = await client.getProof(getProofWithStorageParams);
 
 const indent = '  ';
 
 await writeFile(filePath, stringify(proof, null, indent));
+await appendFile(filePath, '\n');
 console.log(`File has been saved: ${filePath}`);
 
 await writeFile(filePathStorage, stringify(proofWithStorage, null, indent));
+await appendFile(filePathStorage, '\n');
 console.log(`File has been saved: ${filePathStorage}`);
