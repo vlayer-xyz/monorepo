d2f7170f250836c038c0e5522bd8ac5b9f163351
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-03-04 11:34:08 +0100

    Refactor and add json formatting

--- ethereum_history_api/oracles/src/script/fetchProofs.ts
+++ ethereum_history_api/oracles/src/script/fetchProofs.ts
@@ -1,18 +1,20 @@
 import { createDefaultClient } from '../ethereum/client.js';
 import { writeFile } from 'fs/promises';
 import { stringify } from '../util/json-bigint.js';
+import { CIRCLE_USDC_BALANCE_STORAGE_KEY } from './prepareNoirFixtures.js';
 import { type GetProofParameters, type GetProofReturnType } from 'viem';
 
 const filePath = './fixtures/eth_getProof_response.json';
+const filePathStorage = './fixtures/eth_getProofWithStorage_response.json';
 
 const getProofParams: GetProofParameters = {
   address: '0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb',
-  storageKeys: [],
+  storageKeys: [CIRCLE_USDC_BALANCE_STORAGE_KEY],
   blockNumber: 14194126n
 };
 
 const client = createDefaultClient();
-console.log('Downloading proof...');
+console.log('Downloading proofs...');
 
 const proof: GetProofReturnType = await client.getProof({
   address: getProofParams.address,
@@ -20,5 +22,14 @@ const proof: GetProofReturnType = await client.getProof({
   blockNumber: getProofParams.blockNumber
 });
 
-await writeFile(filePath, stringify(proof));
+const proofWithStorage: GetProofReturnType = await client.getProof({
+  address: getProofParams.address,
+  storageKeys: getProofParams.storageKeys,
+  blockNumber: getProofParams.blockNumber
+});
+
+await writeFile(filePath, stringify(proof, null, '  '));
 console.log(`File has been saved: ${filePath}`);
+
+await writeFile(filePathStorage, stringify(proofWithStorage, null, '  '));
+console.log(`File has been saved: ${filePathStorage}`);
