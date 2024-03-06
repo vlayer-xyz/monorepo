dddc05bfdd374c322afb101e54754216a8d95913
Leonid Logvinov <logvinov.leon@gmail.com>
2024-03-06 15:15:53 +0100

    Rename new_fixtures to fixtures as all fixtures are migrated

--- ethereum_history_api/oracles/src/fixtures/config.ts
+++ ethereum_history_api/oracles/src/fixtures/config.ts
@@ -1,6 +1,6 @@
 import { Address, Hex } from 'viem';
 
-export const JS_FIXTURES_DIRECTORY = 'new_fixtures';
+export const JS_FIXTURES_DIRECTORY = 'fixtures';
 export const CIRCLE_USDC_BALANCE_STORAGE_KEY = '0x57d18af793d7300c4ba46d192ec7aa095070dde6c52c687c6d0d92fb8532b305';
 
 interface Fixture {
