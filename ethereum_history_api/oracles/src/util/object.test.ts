9fb406bb2892c763eb16e17e2971583e31fc6c5d
Leonid Logvinov <logvinov.leon@gmail.com>
2024-03-06 12:10:26 +0100

    Refactor updateNestedField

--- ethereum_history_api/oracles/src/util/object.test.ts
+++ ethereum_history_api/oracles/src/util/object.test.ts
@@ -7,13 +7,6 @@ describe('updateNestedField', () => {
     updateNestedField(object, ['a', '0', 'bar', 'c'], (x: number) => x + 1);
     expect(object.a[0].bar.c).to.eq(4);
   });
-
-  it('non-existing key', () => {
-    const object = { a: [{ bar: { c: 3 } }] };
-    updateNestedField(object, ['x', '0', 'y', 'z'], () => 5);
-    /* eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access */
-    expect((object as any).x[0]?.y?.z).to.eq(5);
-  });
 });
 
 describe('copy', () => {
--- ethereum_history_api/oracles/src/util/object.ts
+++ ethereum_history_api/oracles/src/util/object.ts
@@ -1,11 +1,16 @@
-/* eslint-disable @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument */
-export function updateNestedField<T, V>(obj: T, pathArray: string[], updater: (value: V) => V): void {
-  /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
-  pathArray.reduce((acc: any, key: string, i: number) => {
-    if (acc[key] === undefined) acc[key] = {};
-    if (i === pathArray.length - 1) acc[key] = updater(acc[key]);
-    return acc[key];
-  }, obj);
+interface NestedObject<V> {
+  [key: string]: NestedObject<V> | V;
 }
 
-export const copy = <T>(obj: T): T => JSON.parse(JSON.stringify(obj));
+export function updateNestedField<T, V>(obj: T, pathArray: string[], updater: (value: V) => V): void {
+  let currentSubtree = obj as NestedObject<V>;
+  const pathWithoutLast = pathArray.slice(0, pathArray.length - 1);
+  const lastPathElem = pathArray[pathArray.length - 1];
+  for (const pathElem of pathWithoutLast) {
+    currentSubtree = currentSubtree[pathElem] as NestedObject<V>;
+  }
+
+  currentSubtree[lastPathElem] = updater(currentSubtree[lastPathElem] as V);
+}
+
+export const copy = <T>(obj: T): T => JSON.parse(JSON.stringify(obj)) as T;
