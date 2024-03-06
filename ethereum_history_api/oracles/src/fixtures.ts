998d6e36e54c3a962c178ff942fbfdd4e9add527
Leonid Logvinov <logvinov.leon@gmail.com>
2024-03-06 17:20:52 +0100

    Extract loadFixture function

--- ethereum_history_api/oracles/src/fixtures.ts
+++ ethereum_history_api/oracles/src/fixtures.ts
@@ -1,17 +1,19 @@
 import { join } from 'path';
 import { GetBlockReturnType, GetProofReturnType } from 'viem';
 import { JS_FIXTURES_DIRECTORY } from './fixtures/config.js';
-import { GetBlockFixture, GetProofFixture } from './fixtures/types.js';
+import { BaseFixture } from './fixtures/types.js';
 import { readObject } from './util/file.js';
 
+export async function loadFixture<T>(hardFork: string, fixtureName: string, method: string): Promise<T> {
+  const fileName = join(JS_FIXTURES_DIRECTORY, hardFork, fixtureName, `${method}.json`);
+  const fixture = await readObject<BaseFixture<T>>(fileName);
+  return fixture.result;
+}
+
 export async function loadBlockFixture(hardFork: string, fixtureName: string): Promise<GetBlockReturnType> {
-  const fileName = join(JS_FIXTURES_DIRECTORY, hardFork, fixtureName, 'eth_getBlockByHash.json');
-  const getBlockFixture = await readObject<GetBlockFixture>(fileName);
-  return getBlockFixture.result;
+  return loadFixture<GetBlockReturnType>(hardFork, fixtureName, 'eth_getBlockByHash');
 }
 
 export async function loadProofFixture(hardFork: string, fixtureName: string): Promise<GetProofReturnType> {
-  const fileName = join(JS_FIXTURES_DIRECTORY, hardFork, fixtureName, 'eth_getProof.json');
-  const getProofFixture = await readObject<GetProofFixture>(fileName);
-  return getProofFixture.result;
+  return loadFixture<GetProofReturnType>(hardFork, fixtureName, 'eth_getProof');
 }
