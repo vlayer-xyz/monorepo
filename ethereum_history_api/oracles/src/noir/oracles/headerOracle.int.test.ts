4a7dd18c7f7f0de5ecaffe8f0eb80cdba78afd05
Leonid Logvinov <logvinov.leon@gmail.com>
2024-03-08 14:27:55 +0100

    Remove timeouts from oracle integration tests as they use mocks now

--- ethereum_history_api/oracles/src/noir/oracles/headerOracle.int.test.ts
+++ ethereum_history_api/oracles/src/noir/oracles/headerOracle.int.test.ts
@@ -4,36 +4,28 @@ import { type ForeignCallOutput } from '@noir-lang/noir_js';
 import { createMockClient } from '../../ethereum/mockClient.js';
 import { FIXTURES } from '../../fixtures/config.js';
 
-describe(
-  'headerOracle',
-  async () => {
-    const mockFilePaths = ['./fixtures/paris/usdc/eth_getBlockByHash.json'];
-    const client = await createMockClient(mockFilePaths);
+describe('headerOracle', async () => {
+  const mockFilePaths = ['./fixtures/paris/usdc/eth_getBlockByHash.json'];
+  const client = await createMockClient(mockFilePaths);
 
-    it('getBlock', async () => {
-      const blockNumber = FIXTURES.paris.usdc.blockNumber;
-      const blockHeader = await getBlockHeader(client, blockNumber);
-      expect(blockHeader.number).toStrictEqual('0x121eac0');
-      expect(blockHeader.parentHash).toStrictEqual(
-        '0x759e27a5069535949f0a7247ebc999367dbd77964d77ed004ffc8db3d4940248'
-      );
-      expect(blockHeader.difficulty).toStrictEqual('0x0');
-    });
+  it('getBlock', async () => {
+    const blockNumber = FIXTURES.paris.usdc.blockNumber;
+    const blockHeader = await getBlockHeader(client, blockNumber);
+    expect(blockHeader.number).toStrictEqual('0x121eac0');
+    expect(blockHeader.parentHash).toStrictEqual('0x759e27a5069535949f0a7247ebc999367dbd77964d77ed004ffc8db3d4940248');
+    expect(blockHeader.difficulty).toStrictEqual('0x0');
+  });
 
-    it('getHeaderOracle', async () => {
-      const blockHeader: ForeignCallOutput[] = await getHeaderOracle(client, [['0x121eac0']]);
-      expect(blockHeader.length === 7);
-      // prettier-ignore
-      const encodedStateRootOfParisBlockBlock = [
+  it('getHeaderOracle', async () => {
+    const blockHeader: ForeignCallOutput[] = await getHeaderOracle(client, [['0x121eac0']]);
+    expect(blockHeader.length === 7);
+    // prettier-ignore
+    const encodedStateRootOfParisBlockBlock = [
         "0x1a","0xd7","0xb8","0x0a","0xf0","0xc2","0x8b","0xc1",
         "0x48","0x95","0x13","0x34","0x6d","0x27","0x06","0x88",
         "0x5b","0xe9","0x0a","0xbb","0x07","0xf2","0x3c","0xa2",
         "0x8e","0x50","0x48","0x2a","0xdb","0x39","0x2d","0x61"
       ];
-      expect(blockHeader[2]).toStrictEqual(encodedStateRootOfParisBlockBlock);
-    });
-  },
-  {
-    timeout: 2000
-  }
-);
+    expect(blockHeader[2]).toStrictEqual(encodedStateRootOfParisBlockBlock);
+  });
+});
