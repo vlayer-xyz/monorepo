f1a3a913ed1258f16a2f55f53e334989c58c9d87
Leonid Logvinov <logvinov.leon@gmail.com>
2024-03-17 11:39:31 +0100

    Refactor receipt proofs to use ReceiptTrie abstraction

--- ethereum_history_api/oracles/src/ethereum/receipt.ts
+++ ethereum_history_api/oracles/src/ethereum/receipt.ts
@@ -3,6 +3,7 @@ import { toHexString } from './blockHeader.js';
 import { AlchemyClient } from './alchemyClient.js';
 import { Trie } from '@ethereumjs/trie';
 import { assert } from '../util/assert.js';
+import { encodeField } from '../noir/oracles/common/encode.js';
 
 export type RecursiveArray<T> = T | RecursiveArray<T>[];
 
@@ -58,20 +59,43 @@ export function encodeReceipt(receipt: TransactionReceipt): Hex {
   return encodedReceipt;
 }
 
-export async function getReceiptProof(client: AlchemyClient, blockNumber: bigint, txIdx: number): Promise<Hex[]> {
-  const trie = new Trie();
+export class ReceiptTrie {
+  private trie: Trie = new Trie();
 
-  const block = await client.getBlock({ blockNumber });
-  const receipts = await client.getTransactionReceipts({ blockNumber });
-
-  for (const [i, receipt] of receipts.entries()) {
-    await trie.put(encode(i), hexToBytes(encodeReceipt(receipt)));
+  public root(): Hex {
+    return bytesToHex(this.trie.root());
   }
 
-  assert(
-    block.receiptsRoot === bytesToHex(trie.root()),
-    `receiptsRoot mismatch: ${block.receiptsRoot} !== ${bytesToHex(trie.root())}`
-  );
+  public async put(txIdx: number, receipt: TransactionReceipt) {
+    const key = ReceiptTrie.keyFromIdx(txIdx);
+    const value = hexToBytes(encodeReceipt(receipt));
+    await this.trie.put(key, value);
+  }
 
-  return (await trie.createProof(encode(txIdx))).map((value) => bytesToHex(value));
+  public async createProof(txIdx: number): Promise<Hex[]> {
+    const key = ReceiptTrie.keyFromIdx(txIdx);
+    const proof = await this.trie.createProof(key);
+    return proof.map((node) => bytesToHex(node));
+  }
+
+  public static keyFromIdx(txIdx: number): Uint8Array {
+    return toRlp(encodeField(txIdx), 'bytes');
+  }
+}
+
+export async function getReceiptTrie(receipts: TransactionReceipt[], expectedRoot: Hex): Promise<ReceiptTrie> {
+  const trie = new ReceiptTrie();
+  for (const [i, receipt] of receipts.entries()) {
+    await trie.put(i, receipt);
+  }
+  assert(expectedRoot === trie.root(), `receiptsRoot mismatch: ${expectedRoot} !== ${trie.root()}`);
+  return trie;
+}
+
+export async function getReceiptProof(client: AlchemyClient, blockNumber: bigint, txIdx: number): Promise<Hex[]> {
+  const block = await client.getBlock({ blockNumber });
+  const receipts = await client.getTransactionReceipts({ blockNumber });
+  const trie = await getReceiptTrie(receipts, block.receiptsRoot);
+
+  return await trie.createProof(txIdx);
 }
