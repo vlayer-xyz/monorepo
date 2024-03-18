dca26106ccddbf9f471563ca3684fd545c38c19d
Leonid Logvinov <logvinov.leon@gmail.com>
2024-03-18 10:29:27 +0100

    Add get_receipt requires 2 arguments test

--- ethereum_history_api/oracles/src/noir/oracles/receiptOracle.test.ts
+++ ethereum_history_api/oracles/src/noir/oracles/receiptOracle.test.ts
@@ -2,10 +2,15 @@ import { describe, expect, it } from 'vitest';
 import { decodeGetReceiptArguments } from './receiptOracle.js';
 
 describe('receiptOracle', () => {
-  it('decodeGetReceiptArguments success', () => {
-    expect(decodeGetReceiptArguments([['0xf'], ['0xb4']])).toStrictEqual({
-      blockNumber: 15n,
-      txId: 180n
+  describe('decodeGetReceiptArguments', () => {
+    it('success', () => {
+      expect(decodeGetReceiptArguments([['0xf'], ['0xb4']])).toStrictEqual({
+        blockNumber: 15n,
+        txId: 180n
+      });
+    });
+    it('get_receipt requires 2 arguments', () => {
+      expect(() => decodeGetReceiptArguments([['0xf']])).toThrow('get_receipt requires 2 arguments');
     });
   });
 });
