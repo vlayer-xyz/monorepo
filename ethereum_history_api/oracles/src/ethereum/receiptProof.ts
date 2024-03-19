eec57894ee384e5cfe8b9cffa70ab3fb64d34c87
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-03-19 10:32:00 +0100

    Update receiptProof.ts

--- ethereum_history_api/oracles/src/ethereum/receiptProof.ts
+++ ethereum_history_api/oracles/src/ethereum/receiptProof.ts
@@ -1,14 +1,14 @@
 import { TransactionReceipt, Hex } from 'viem';
-import { assert } from 'vitest';
 import { AlchemyClient } from './client.js';
 import { ReceiptTrie } from './receiptTrie.js';
 import { encodeReceipt } from './receipt.js';
+import { assert } from '../util/assert.js';
 
-type ReceiptProof = {
+export interface ReceiptProof {
   key: Uint8Array;
   proof: Hex[];
   value: Hex;
-};
+}
 
 export async function getReceiptTrie(receipts: TransactionReceipt[], expectedRoot: Hex): Promise<ReceiptTrie> {
   const trie = new ReceiptTrie();
@@ -35,22 +35,4 @@ export async function getReceiptProof(
     proof,
     value: encodeReceipt(receipts[txIdx])
   };
-}
-
-export async function getReceiptProofs(
-  client: AlchemyClient,
-  blockNumber: bigint,
-  txIndexes: number[]
-): Promise<ReceiptProof[]> {
-  const block = await client.getBlock({ blockNumber });
-  const receipts = await client.getTransactionReceipts({ blockNumber });
-  const trie = await getReceiptTrie(receipts, block.receiptsRoot);
-
-  return await Promise.all(
-    txIndexes.map(async (txIdx) => ({
-      key: ReceiptTrie.keyFromIdx(txIdx),
-      proof: await trie.createProof(txIdx),
-      value: encodeReceipt(receipts[txIdx])
-    }))
-  );
 }
