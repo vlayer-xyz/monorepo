9cde740940afa1beb421b696b6e34b02c6b609a9
Arkadiusz Konior <akonior@gmail.com>
2024-03-22 14:51:01 +0100

    Remove MAX_TRANSACTION_DATA_SIZE

--- ethereum_history_api/oracles/src/noir/oracles/transactionOracle/encode.ts
+++
@@ -1,1 +0,0 @@
-export const MAX_TRANSACTION_DATA_SIZE = 1024;
--- ethereum_history_api/oracles/src/script/noir_fixtures/transaction.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/transaction.ts
@@ -1,16 +1,11 @@
 import { GetTransactionReturnType, hexToNumber } from 'viem';
 import { encodeHexString, encodeOptional, joinArray } from '../../noir/noir_js/encode.js';
-import { ZERO_PAD_VALUE } from '../../noir/oracles/common/const.js';
 import { encodeAddress, encodeField } from '../../noir/oracles/common/encode.js';
-import { MAX_TRANSACTION_DATA_SIZE } from '../../noir/oracles/transactionOracle/encode.js';
-import { padArray } from '../../util/array.js';
 
 export function createTransactionFixture(tx: GetTransactionReturnType): string {
   const gasPrice = encodeOptional(tx.gasPrice?.toString());
   const to = encodeOptional(tx.to ? joinArray(encodeAddress(tx.to)) : undefined);
-  const data = padArray(encodeHexString(tx.input), MAX_TRANSACTION_DATA_SIZE, ZERO_PAD_VALUE).map((it) =>
-    hexToNumber(it).toString()
-  );
+  const data = encodeHexString(tx.input).map((it) => hexToNumber(it).toString());
   const dataLen = encodeField(tx.input.length);
   const v = encodeField(tx.v);
   const r = encodeHexString(tx.r);
