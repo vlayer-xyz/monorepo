1c47cecba4f36389d25f816976bfbc5d274ffa22
Leonid Logvinov <logvinov.leon@gmail.com>
2024-03-25 12:34:22 +0100

    Right pad rlp receipt

--- ethereum_history_api/oracles/src/script/noir_fixtures/receipt.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/receipt.ts
@@ -2,9 +2,12 @@ import { TransactionReceipt } from 'viem';
 import { encodeHex } from '../../noir/oracles/common/encode.js';
 import { encodeReceipt, statusToField } from '../../ethereum/receipt.js';
 import { encodeOptional, joinArray, tabulateStructField } from '../../noir/noir_js/encode.js';
+import { padArray } from '../../util/array.js';
+import { ZERO_PAD_VALUE } from '../../noir/oracles/common/const.js';
+import { MAX_RECEIPT_RLP_LENGTH } from '../../noir/oracles/receiptOracle/encode.js';
 
 export function createReceiptFixture(receipt: TransactionReceipt): string {
-  const rlpReceipt = joinArray(encodeHex(encodeReceipt(receipt)));
+  const rlpReceipt = joinArray(padArray(encodeHex(encodeReceipt(receipt)), MAX_RECEIPT_RLP_LENGTH, ZERO_PAD_VALUE));
 
   const status = encodeOptional(statusToField(receipt.status).toString());
   const stateRoot = encodeOptional(receipt.root ? joinArray(encodeHex(receipt.root)) : receipt.root);
