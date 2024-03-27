257b23cb7e74c4a05e8d18d28a8e67b6f03cc346
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-03-27 12:35:29 +0100

    Change tx and receipt key length to be 3 (from 4)

--- ethereum_history_api/oracles/src/script/noir_fixtures/receipt_proof.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/receipt_proof.ts
@@ -1,15 +1,15 @@
 import { Proof } from '../../ethereum/proof.js';
 import {
+  MAX_RECEIPT_KEY_NIBBLE_LEN,
   MAX_RECEIPT_PROOF_LEN,
-  MAX_RECEIPT_RLP_LEN,
-  MAX_RECEIPT_TREE_DEPTH
+  MAX_RECEIPT_RLP_LEN
 } from '../../noir/oracles/accountOracle/encode.js';
 import { createProofFixture } from './proof.js';
 
 export function createReceiptProofFixture(proof: Proof): string {
   return createProofFixture(
     proof,
-    MAX_RECEIPT_TREE_DEPTH,
+    MAX_RECEIPT_KEY_NIBBLE_LEN,
     MAX_RECEIPT_RLP_LEN,
     MAX_RECEIPT_PROOF_LEN,
     'crate::receipt::TxReceiptProof',
--- ethereum_history_api/oracles/src/script/noir_fixtures/transaction_proof.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/transaction_proof.ts
@@ -2,14 +2,14 @@ import { Proof } from '../../ethereum/proof.js';
 import {
   MAX_TRANSACTION_PROOF_LEN,
   MAX_TRANSACTION_RLP_LEN,
-  MAX_TRANSACTION_TREE_DEPTH
+  MAX_TX_KEY_NIBBLE_LEN
 } from '../../noir/oracles/accountOracle/encode.js';
 import { createProofFixture } from './proof.js';
 
 export function createTransactionProofFixture(proof: Proof): string {
   return createProofFixture(
     proof,
-    MAX_TRANSACTION_TREE_DEPTH,
+    MAX_TX_KEY_NIBBLE_LEN,
     MAX_TRANSACTION_RLP_LEN,
     MAX_TRANSACTION_PROOF_LEN,
     'crate::transaction::TransactionProof',
