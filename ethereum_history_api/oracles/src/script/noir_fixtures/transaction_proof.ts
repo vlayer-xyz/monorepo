3aead9c5008b56ed5542b49f514d73a86c44b703
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-03-27 16:54:20 +0100

    Refactor

--- ethereum_history_api/oracles/src/script/noir_fixtures/transaction_proof.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/transaction_proof.ts
@@ -1,17 +1,13 @@
 import { Proof } from '../../ethereum/proof.js';
-import {
-  MAX_TRANSACTION_PROOF_LEN,
-  MAX_TRANSACTION_RLP_LEN,
-  MAX_TX_KEY_NIBBLE_LEN
-} from '../../noir/oracles/accountOracle/encode.js';
+import { MAX_TX_PROOF_LEN, MAX_TX_RLP_LEN, MAX_TX_KEY_NIBBLE_LEN } from '../../noir/oracles/accountOracle/encode.js';
 import { createProofFixture } from './proof.js';
 
 export function createTransactionProofFixture(proof: Proof): string {
   return createProofFixture(
     proof,
     MAX_TX_KEY_NIBBLE_LEN,
-    MAX_TRANSACTION_RLP_LEN,
-    MAX_TRANSACTION_PROOF_LEN,
+    MAX_TX_RLP_LEN,
+    MAX_TX_PROOF_LEN,
     'crate::transaction::TransactionProof',
     'TransactionProof'
   );
