8c2bcb51915524f1f8ad78a75fb8c11328665d24
Leonid Logvinov <logvinov.leon@gmail.com>
2024-03-28 15:25:40 +0100

    More tests for padHexToEven

--- ethereum_history_api/oracles/src/util/hex.test.ts
+++ ethereum_history_api/oracles/src/util/hex.test.ts
@@ -10,19 +10,22 @@ describe('hex', () => {
   it('removePrefix', () => {
     expect(removeHexPrefix('0x01')).toEqual('01');
 
-    expect(() => removeHexPrefix('01')).toThrow('Expected hex to have a prefix: 01');
+    expect(() => removeHexPrefix('01')).toThrow('Attempting to remove 0x prefix from 01 which does not have it');
   });
 
   it('addPrefix', () => {
     expect(addHexPrefix('1')).toEqual('0x1');
     expect(addHexPrefix('01')).toEqual('0x01');
 
-    expect(() => addHexPrefix('0x01')).toThrow('Expected hex to not have a prefix: 0x01');
+    expect(() => addHexPrefix('0x01')).toThrow('Attempting to add 0x prefix to 0x01 which already has it');
   });
 
   it('padHexToEven', () => {
+    expect(padHexToEven('')).toEqual('');
     expect(padHexToEven('1')).toEqual('01');
+    expect(padHexToEven('01')).toEqual('01');
     expect(padHexToEven('0x1')).toEqual('0x01');
     expect(padHexToEven('0x01')).toEqual('0x01');
+    expect(padHexToEven('0x01234')).toEqual('0x001234');
   });
 });
