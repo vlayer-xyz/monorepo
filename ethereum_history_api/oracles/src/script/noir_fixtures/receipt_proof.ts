368e3f5584ba1c62b177516fbbafd962516e4451
Arkadiusz Konior <akonior@gmail.com>
2024-04-04 11:39:28 +0200

    Verify tx idx (#193)
    * Add assert_tx_idx_equals to verify_receipt function
    
    * Add tx_idx to fixtures
    
    * Update proper max value len in generating fixtures
    
    * Add test_verify_receipt, test_verify_receipt_invalid_tx_idx
    
    * Add all verify_receipt tests

--- ethereum_history_api/oracles/src/script/noir_fixtures/receipt_proof.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/receipt_proof.ts
@@ -1,8 +1,8 @@
 import { Proof } from '../../ethereum/proof.js';
 import {
+  MAX_RECEIPT_ENCODED_LEN,
   MAX_RECEIPT_KEY_NIBBLE_LEN,
-  MAX_RECEIPT_PROOF_LEN,
-  MAX_RECEIPT_RLP_LEN
+  MAX_RECEIPT_PROOF_LEN
 } from '../../noir/oracles/receiptOracle/encode.js';
 import { createProofFixture } from './proof.js';
 
@@ -10,7 +10,7 @@ export function createReceiptProofFixture(proof: Proof): string {
   return createProofFixture(
     proof,
     MAX_RECEIPT_KEY_NIBBLE_LEN,
-    MAX_RECEIPT_RLP_LEN,
+    MAX_RECEIPT_ENCODED_LEN,
     MAX_RECEIPT_PROOF_LEN,
     'dep::proof::trie_proof::TrieProof',
     'TrieProof'
