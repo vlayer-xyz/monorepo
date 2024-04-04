a8e4d11975251fb991eea9b924b95e849fe6bb82
Arkadiusz Konior <akonior@gmail.com>
2024-04-04 10:49:58 +0200

    Update proper max value len in generating fixtures

--- ethereum_history_api/oracles/src/script/noir_fixtures/proof.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/proof.ts
@@ -9,14 +9,14 @@ import { BYTE_HEX_LEN } from '../../util/const.js';
 export function createProofFixture(
   proof: Proof,
   maxTreeDepth: number,
-  maxRlpLen: number,
+  maxValueLen: number,
   maxProofLen: number,
   crateImport: string,
   proofNoirType: string
 ): string {
   const paddedKey = padHex(proof.key, { size: maxTreeDepth / BYTE_HEX_LEN, dir: 'left' });
   const key = encodeHex(paddedKey);
-  const value = padArray(encodeHexString(proof.value), maxRlpLen, ZERO_PAD_VALUE, 'left');
+  const value = padArray(encodeHexString(proof.value), maxValueLen, ZERO_PAD_VALUE, 'left');
   const encodedProof = encodeProof(proof.proof, maxProofLen);
   const depth = proof.proof.length;
   return `use ${crateImport};
