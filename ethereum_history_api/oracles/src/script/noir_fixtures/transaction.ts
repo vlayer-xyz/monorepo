297e2640c254f867d307decd886143b3d0b70b9c
Leonid Logvinov <logvinov.leon@gmail.com>
2024-04-08 16:46:07 +0200

    Add tx_rlp, encoded_tx and tx_type to tx noir fixtures

--- ethereum_history_api/oracles/src/script/noir_fixtures/transaction.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/transaction.ts
@@ -1,10 +1,20 @@
-import { GetTransactionReturnType } from 'viem';
+import { GetTransactionReturnType, toRlp } from 'viem';
 import { encodeOptional, joinArray, indentBlock } from '../../noir/noir_js/encode.js';
 import { encodeAddress, encodeField, encodeHex } from '../../noir/oracles/common/encode.js';
 import { BYTE_HEX_LEN } from '../../util/const.js';
 import { removeHexPrefix } from '../../util/hex.js';
+import { txTypeToField } from '../../ethereum/receipt.js';
+import { padArray } from '../../util/array.js';
+import { TxRlpEncoder, encodeTx } from '../../ethereum/transaction.js';
+import { MAX_TX_RLP_LEN, MAX_TX_ENCODED_LEN } from '../../noir/oracles/transactionOracle/encode.js';
+import { ZERO_PAD_VALUE } from '../../noir/oracles/common/const.js';
 
 export function createTransactionFixture(tx: GetTransactionReturnType): string {
+  const fields = TxRlpEncoder.txToFields(tx);
+  const hx = encodeHex(toRlp(fields));
+  const txRlp = joinArray(padArray(hx, MAX_TX_RLP_LEN, ZERO_PAD_VALUE));
+  const encodedTx = joinArray(padArray(encodeHex(encodeTx(tx)), MAX_TX_ENCODED_LEN, ZERO_PAD_VALUE));
+
   const txIdx = encodeField(tx.transactionIndex);
   const to = encodeOptional(tx.to ? joinArray(encodeAddress(tx.to)) : undefined);
   const data = encodeHex(tx.input);
@@ -16,6 +26,10 @@ export function createTransactionFixture(tx: GetTransactionReturnType): string {
   return `use crate::transaction::TxPartial;
 
 global tx_idx = ${txIdx};
+
+global tx_type = ${txTypeToField(tx.type)};
+global tx_rlp = ${txRlp};
+global encoded_tx = ${encodedTx};
 
 global transaction = TxPartial {
   nonce: ${tx.nonce},
