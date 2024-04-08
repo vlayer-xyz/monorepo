3f9f0daca003ac81f4726a9e8e572b681b070b3e
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-04-08 14:33:58 +0200

    Initial version of preparing merkle proofs fixtures

--- ethereum_history_api/oracles/src/script/noir_fixtures/merkleProof.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/merkleProof.ts
@@ -1,24 +1,23 @@
-import { Trie } from '@ethereumjs/trie';
+import { Proof, Trie } from '@ethereumjs/trie';
 import { encodeHex, encodeUint8ArrayToBytes } from '../../noir/oracles/common/encode.js';
 import { encodeHexStringToArray } from '../../main.js';
-import { ProofInput } from '../prepareProofFixtures.js';
 import { joinArray } from '../../noir/noir_js/encode.js';
+import { Hex } from 'viem';
 
-const exampleValue = encodeHexStringToArray('0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef');
+interface ProofFixture {
+  root: Uint8Array;
+  key: Hex;
+  value: None; // TODO: take calue from prepareMerkleqProofFixtures.ts
+  nodes: Uint8Array[];
+  leaf: Uint8Array;
+}
 
-export async function createMerkleProofFixture(proofInput: ProofInput) {
-  const trie = new Trie();
-  for (const keyValuePair of proofInput.keyValuePairs) {
-    await trie.put(encodeHexStringToArray(keyValuePair.key), encodeHexStringToArray(keyValuePair.value));
-  }
-
-  const key = proofInput.key;
-  const proof = await trie.createProof(encodeHexStringToArray(key));
-  const nodes = proof.slice(0, proof.length - 1);
-  const leaf = proof[proof.length - 1];
-
-  return `global root = ${joinArray(encodeUint8ArrayToBytes(trie.root()))};
-global key = ${joinArray(encodeHex(key))};
+export async function createMerkleProofFixture(proofFixture: ProofFixture) {
+  const root = encodeUint8ArrayToBytes(proofFixture.root);
+  const key = encodeHex(proofFixture.key);
+  const value = encodeUint8ArrayToBytes(exampleValue);
+  return `global root = ${joinArray()};
+global key = ${joinArray()};
 global value = ${joinArray(encodeUint8ArrayToBytes(exampleValue))};
 global nodes = [${nodes.length > 0 ? '[' + nodes.map((proofOneLevel) => joinArray(encodeUint8ArrayToBytes(proofOneLevel))).join('], [') + ']' : ''}];
 global leaf = ${joinArray(encodeUint8ArrayToBytes(leaf))};
--- ethereum_history_api/oracles/src/script/prepareMerkleProofFixtures.ts
+++ ethereum_history_api/oracles/src/script/prepareMerkleProofFixtures.ts
@@ -1,7 +1,26 @@
 import { writeFile } from 'fs/promises';
 import { join } from 'path';
 import { createMerkleProofFixture } from './noir_fixtures/merkleProof.js';
+import { Trie } from '@ethereumjs/trie';
+import { PROOF_FIXTURES } from '../fixtures/merkleProofsConfig.js';
+import { encodeHexStringToArray } from '../main.js';
 
-const NOIR_FIXTURES_DIRECTORY = '../circuits/lib/src/fixtures';
+const NOIR_PROOF_FIXTURES_DIRECTORY = '../circuits/lib/src/fixtures/merkle_proofs';
 
-// await writeFile(join(NOIR_FIXTURES_DIRECTORY, 'proof.nr'), await createMerkleProofFixture(null));
+for (const fixtureName in PROOF_FIXTURES) {
+  const trie = new Trie();
+  for (const keyValuePair of PROOF_FIXTURES[fixtureName].keyValuePairs) {
+    await trie.put(encodeHexStringToArray(keyValuePair.key), encodeHexStringToArray(keyValuePair.value));
+  }
+
+  // TODO:
+  // 1. check if key in keyValuePairs
+  // 2. find value corresponding to key
+  const key = PROOF_FIXTURES[fixtureName].key;
+  const proof = await trie.createProof(encodeHexStringToArray(key));
+  const root = trie.root();
+  const nodes = proof.slice(0, proof.length - 1);
+  const leaf = proof[proof.length - 1];
+
+  await writeFile(`${NOIR_PROOF_FIXTURES_DIRECTORY}/${fixtureName}.nr`, await createMerkleProofFixture(null));
+}
