80657632ce3d11a4b2166f28f7e98612440fc7d8
Leonid Logvinov <logvinov.leon@gmail.com>
2024-04-09 10:11:11 +0200

    Assert tx proof with tests (#201)
    * Use TrieProof type from the dep instead of a custom type in fixtures
    
    * Update proof lib version
    
    * Fix noir fixtures indentation
    
    * Fix MAX_TX_RLP_LEN in noir fixtures
    
    * Add assert_tx_proof with tests
    
    * Update test fixtures
    
    * Add a test for leaf node proof failure

--- ethereum_history_api/oracles/src/script/noir_fixtures/storage_proof.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/storage_proof.ts
@@ -1,5 +1,5 @@
 import { Hash } from 'viem';
-import { encodeHexString, joinArray } from '../../noir/noir_js/encode.js';
+import { encodeHexString, indentBlock, joinArray } from '../../noir/noir_js/encode.js';
 import { encodeBytes32, encodeProof } from '../../noir/oracles/common/encode.js';
 import { STORAGE_PROOF_LEN } from '../../noir/oracles/accountOracle/encode.js';
 
@@ -22,12 +22,11 @@ function createSingleStorageProofFixture(storageProof: StorageProof): string {
   const value = encodeBytes32(storageProof.value);
   const proof = encodeProof(storageProof.proof, STORAGE_PROOF_LEN);
   const depth = storageProof.proof.length;
-  const storageProofFixture = `
-  StorageProof {
-    key: ${joinArray(key)},
-    value: ${joinArray(value)},
-    proof: ${joinArray(proof)},
-    depth: ${depth}
-  }`;
-  return storageProofFixture;
+  const storageProofFixture = `StorageProof {
+  key: ${indentBlock(joinArray(key), 1)},
+  value: ${indentBlock(joinArray(value), 1)},
+  proof: ${indentBlock(joinArray(proof), 1)},
+  depth: ${depth}
+}`;
+  return indentBlock(storageProofFixture, 1);
 }
