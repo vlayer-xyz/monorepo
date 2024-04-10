f0e0165342770425e3163917a0afb5cbb5c947d1
Leonid Logvinov <logvinov.leon@gmail.com>
2024-04-10 16:33:22 +0200

    Adjust tx proof fixtures value size

--- ethereum_history_api/oracles/src/script/noir_fixtures/transaction_proof.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/transaction_proof.ts
@@ -2,7 +2,7 @@ import { Proof } from '../../ethereum/proof.js';
 import {
   MAX_TX_KEY_NIBBLE_LEN,
   MAX_TX_PROOF_LEN,
-  MAX_TX_RLP_LEN
+  MAX_TX_ENCODED_LEN
 } from '../../noir/oracles/transactionOracle/encode.js';
 import { createProofFixture } from './proof.js';
 
@@ -10,7 +10,7 @@ export function createTransactionProofFixture(proof: Proof): string {
   return createProofFixture(
     proof,
     MAX_TX_KEY_NIBBLE_LEN,
-    MAX_TX_RLP_LEN,
+    MAX_TX_ENCODED_LEN,
     MAX_TX_PROOF_LEN,
     'dep::proof::trie_proof::TrieProof',
     'TrieProof'
