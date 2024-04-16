9803506381161b13ef9fe299007c260c741d602c
Izabela Ożdżeńska <63781356+Pasifaee@users.noreply.github.com>
2024-04-16 11:20:46 +0200

    Implement initial check for key matching in merkle_patricia_proofs & expand Fragment's API (#221)
    * Add bytes_to_nibbles_fragment function
    
    * Add to fragment's API
    
    * Rewrite strip_prefix function
    
    * Add fragment tests
    
    * Update merkle proofs test preparation to left-pad key
    
    * Implement initial key check

--- ethereum_history_api/oracles/src/script/noir_fixtures/merkle_proof.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/merkle_proof.ts
@@ -1,6 +1,9 @@
 import { encodeHex, encodeUint8Array } from '../../noir/oracles/common/encode.js';
 import { indentBlock, joinArray } from '../../noir/noir_js/encode.js';
 import { Hex } from 'viem';
+import { padArray } from '../../util/array.js';
+import { ZERO_PAD_VALUE } from '../../noir/oracles/common/const.js';
+import { BYTE_HEX_LEN } from '../../util/const.js';
 
 interface ProofFixture {
   key: Hex;
@@ -13,7 +16,9 @@ interface ProofFixture {
 }
 
 export function createMerkleProofFixture(proofFixture: ProofFixture) {
-  const key = joinArray(encodeHex(proofFixture.key));
+  const keyHex = encodeHex(proofFixture.key);
+  const maxPrefixedKeyNibbleLen = (keyHex.length + 1) * BYTE_HEX_LEN;
+  const key = joinArray(padArray(keyHex, maxPrefixedKeyNibbleLen, ZERO_PAD_VALUE, 'left'));
   const value = joinArray(encodeHex(proofFixture.value));
   const root = joinArray(encodeUint8Array(proofFixture.root));
   const nodes = joinArray(proofFixture.proof.nodes.map((node) => indentBlock(joinArray(encodeUint8Array(node)), 1)));
