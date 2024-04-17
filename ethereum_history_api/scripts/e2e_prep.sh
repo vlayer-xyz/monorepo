824705dc2fad570601dc50f24a9da0aa04b9cfd8
Leonid Logvinov <logvinov.leon@gmail.com>
2024-04-17 20:33:51 +0200

    Add e2e tests for receipt (#226)

--- ethereum_history_api/scripts/e2e_prep.sh
+++ ethereum_history_api/scripts/e2e_prep.sh
@@ -1,6 +1,7 @@
 nargo compile --package get_header
 nargo compile --package get_account
 nargo compile --package get_storage
+nargo compile --package get_receipt
 
 echo "Prove get_header"
 nargo prove --package get_header --oracle-resolver=http://localhost:5555
@@ -8,6 +9,8 @@ echo "Prove get_account"
 nargo prove --package get_account --oracle-resolver=http://localhost:5555
 echo "Prove get_storage"
 nargo prove --package get_storage --oracle-resolver=http://localhost:5555
+echo "Prove get_receipt"
+nargo prove --package get_receipt --oracle-resolver=http://localhost:5555
 
 echo "Codegen get_header"
 nargo codegen-verifier --package get_header
@@ -15,3 +18,5 @@ echo "Codegen get_account"
 nargo codegen-verifier --package get_account
 echo "Codegen get_storage"
 nargo codegen-verifier --package get_storage
+echo "Codegen get_receipt"
+nargo codegen-verifier --package get_receipt
---
+++ ethereum_history_api/tests/src/get_receipt.e2e.test.ts
@@ -0,0 +1,38 @@
+import { beforeEach, describe, expect, it } from 'vitest';
+import { updateNestedField, incHexStr } from 'noir-ethereum-api-oracles';
+import { Abi, InputMap, abiEncode } from '@noir-lang/noirc_abi';
+
+import getReceiptVerifier from '../../contracts/out/GetReceiptUltraPLONKVerifier.sol/UltraVerifier.json';
+import getReceipt from '../../../target/get_receipt.json';
+
+import { readProofData } from './proofDataReader.js';
+import { FoundryArtefact, deploySolidityProofVerifier } from './solidityVerifier.js';
+
+export const abi = getReceipt.abi as unknown as Abi;
+
+describe('get_receipt', async () => {
+  let proof: Uint8Array;
+  let inputMap: InputMap;
+  const proofVerifier = await deploySolidityProofVerifier(getReceiptVerifier as FoundryArtefact);
+
+  beforeEach(async () => {
+    ({ proof, inputMap } = await readProofData('get_receipt'));
+  });
+
+  it('proof verification successes', async () => {
+    const witnessMap = abiEncode(abi, inputMap, inputMap.return);
+    expect(await proofVerifier.verify(proof, witnessMap)).toEqual(true);
+  });
+
+  it('proof fails: invalid receipt value', async () => {
+    updateNestedField(inputMap, ['return', 'receipt', 'cumulative_gas_used'], incHexStr);
+    const witnessMapInvalidReceiptValue = abiEncode(abi, inputMap, inputMap.return);
+    expect(await proofVerifier.verify(proof, witnessMapInvalidReceiptValue)).toEqual(false);
+  });
+
+  it('proof fails: invalid block hash', async () => {
+    updateNestedField(inputMap, ['return', 'block_hash', '0'], incHexStr);
+    const witnessMapInvalidBlockHash = abiEncode(abi, inputMap, inputMap.return);
+    expect(await proofVerifier.verify(proof, witnessMapInvalidBlockHash)).toEqual(false);
+  });
+});
