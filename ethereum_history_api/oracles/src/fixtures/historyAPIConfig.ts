94fcebe0697106cf1f5fc714b994a92c99d60c2d
Arkadiusz Konior <akonior@gmail.com>
2024-04-18 12:55:05 +0200

    Bored Ape index 7 holder storage key Noir fixture

--- ethereum_history_api/oracles/src/fixtures/historyAPIConfig.ts
+++ ethereum_history_api/oracles/src/fixtures/historyAPIConfig.ts
@@ -3,6 +3,7 @@ import { Address, Hex } from 'viem';
 export const JS_FIXTURES_DIRECTORY = 'fixtures';
 export const CIRCLE_USDC_BALANCE_STORAGE_KEY = '0x57d18af793d7300c4ba46d192ec7aa095070dde6c52c687c6d0d92fb8532b305';
 export const UNISWAP_V3_USDC_BALANCE_STORAGE_KEY = '0x1f21a62c4538bacf2aabeca410f0fe63151869f172e03c0e00357ba26a341eff';
+export const BORED_APE_7_HOLDER_STORAGE_KEY = '0x405787fa12a823e0f2b7631cc41b3ba8828b3321ca811111fa75cd3aa3bb5add';
 export const CHAIN_LINK_TRANSFER_TX_HASH = '0x98e19df80eb8feae436896cc7cc6d4a97818e6010b56a249352b9ac2caf0d573';
 export const ETH_TRANSFER_TX_HASH = '0xfade98d8c3b7438655139080c33ccd0b1fc5d08b93dab0c3792d6c1001d56ddb';
 export const ORACLIZE_TRANSFER_TX_HASH = '0x38f299591902bfada359527fa6b9b597a959c41c6f72a3b484807fbf52dc8abe';
@@ -58,6 +59,11 @@ export const HISTORY_API_FIXTURES: HistoryAPIFixtures = {
         blockNumber: 19_000_000n,
         address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
         storageKeys: [UNISWAP_V3_USDC_BALANCE_STORAGE_KEY]
+      },
+      bored_ape_yacht_club: {
+        blockNumber: 19_000_000n,
+        address: '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d',
+        storageKeys: [BORED_APE_7_HOLDER_STORAGE_KEY]
       }
     },
     cancun: {
