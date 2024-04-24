f2d72a65449890568b0132686489ad0b391422dc
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-04-24 14:24:12 +0200

    Reqrite createMerkleProofFixture

--- ethereum_history_api/oracles/src/script/noir_fixtures/merkle_proof.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/merkle_proof.ts
@@ -2,34 +2,36 @@ import { encodeHex } from '../../noir/oracles/common/encode.js';
 import { indentBlock, joinArray } from '../../noir/noir_js/encode.js';
 import { padArray } from '../../util/array.js';
 import { MAX_TRIE_NODE_LEN, ZERO_PAD_VALUE } from '../../noir/oracles/common/const.js';
-import { BYTE_HEX_LEN } from '../../util/const.js';
 import { Proof } from '../../ethereum/proof.js';
+import { padHex } from 'viem';
 
-export function createMerkleProofFixture(proof: Proof) {
-  const keyHex = encodeHex(proof.key);
-  const maxPrefixedKeyNibbleLen = (keyHex.length + 1) * BYTE_HEX_LEN;
-  const key = indentBlock(joinArray(padArray(keyHex, maxPrefixedKeyNibbleLen, ZERO_PAD_VALUE, 'left')), 1);
-  const value = indentBlock(joinArray(encodeHex(proof.value)), 1);
-  const proof_nodes = proof.proof.slice(0, proof.proof.length - 1);
-  const proof_leaf = proof.proof[proof.proof.length - 1];
-  const nodes = indentBlock(
-    joinArray(
-      proof_nodes.map((node) => indentBlock(joinArray(padArray(encodeHex(node), MAX_TRIE_NODE_LEN, ZERO_PAD_VALUE)), 1))
-    ),
-    1 + 1
+export function createMerkleProofFixture(
+  proof: Proof,
+  maxPrefixedKeyNibbleLen: number,
+  maxValueLen: number,
+  maxLeafLen: number,
+  maxDepth: number
+) {
+  const paddedKey = padHex(proof.key, { size: maxPrefixedKeyNibbleLen, dir: 'left' });
+  const key = encodeHex(paddedKey);
+  const paddedValue = padHex(proof.value, { size: maxValueLen, dir: 'left' });
+  const value = encodeHex(paddedValue);
+  const proof_nodes = padArray(proof.proof.slice(0, proof.proof.length - 1), maxDepth, ZERO_PAD_VALUE);
+  const nodes = proof_nodes.map((node) =>
+    indentBlock(joinArray(padArray(encodeHex(node), MAX_TRIE_NODE_LEN, ZERO_PAD_VALUE)), 1)
   );
-  const leaf = indentBlock(joinArray(encodeHex(proof_leaf)), 1 + 1);
+  const leaf = padArray(encodeHex(proof.proof[proof.proof.length - 1]), maxLeafLen, ZERO_PAD_VALUE);
   const depth = proof.proof.length;
 
   return `use crate::merkle_patricia_proofs::proof::{Proof, ProofInput};
 
 global proof_input = ProofInput {\n
-  key: ${key},
-  value: ${value},
+  key: ${indentBlock(joinArray(key), 1)},
+  value: ${indentBlock(joinArray(value), 1)},
 
   proof: Proof {
-    nodes: ${nodes},
-    leaf: ${leaf},
+    nodes: ${indentBlock(joinArray(nodes), 1 + 1)},
+    leaf: ${indentBlock(joinArray(leaf), 1 + 1)},
     depth: ${depth}
   }
 };
