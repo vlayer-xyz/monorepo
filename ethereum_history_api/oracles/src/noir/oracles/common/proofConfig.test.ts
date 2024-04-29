e24f0cdc4b42298ffcf79b5d62ed0dd235d0e406
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-04-29 10:34:04 +0200

    Add getProofConfig test name

--- ethereum_history_api/oracles/src/noir/oracles/common/proofConfig.test.ts
+++ ethereum_history_api/oracles/src/noir/oracles/common/proofConfig.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 import { getProofConfig } from './proofConfig.js';
 
 describe('getProofConfig', () => {
-  it('', () => {
+  it('simple', () => {
     const maxKeyLen = 3;
     const maxValueLen = 110;
     const maxProofLevels = 11;
