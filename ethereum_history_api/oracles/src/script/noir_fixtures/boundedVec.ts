c117530aa9417df6a895c23e386304bd5dcdc569
Anna Jablonowska <jablonowska999@gmail.com>
2024-05-09 15:16:03 +0200

    Update fixtures

--- ethereum_history_api/oracles/src/script/noir_fixtures/boundedVec.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/boundedVec.ts
@@ -1,6 +1,9 @@
 import { Hex } from 'viem';
 import { indentBlock, joinArray, joinArrayVertical } from '../../noir/noir_js/encode.js';
 import { encodeHex } from '../../noir/oracles/common/encode.js';
+import { ZERO_PAD_VALUE } from '../../noir/oracles/common/const.js';
+import { padArray } from '../../util/array.js';
+import { BYTE_HEX_LEN } from '../../util/const.js';
 
 export function createBoundedVecFixture(value: Hex): string {
   const valueAsFields = encodeHex(value);
@@ -17,9 +20,13 @@ export function createVerticalBoundedVecFixture(values: string[]): string {
 }`;
 }
 
-export function createBoundedVecFixtureFromArray(values: string[]): string {
+export function createBoundedVecFixtureWithLen(value: Hex, max_len: number): string {
+  const prefixLen = 2; // 0x
+  const dataLen = (value.length - prefixLen) / BYTE_HEX_LEN;
+  const input = joinArray(padArray(encodeHex(value), max_len, ZERO_PAD_VALUE));
+
   return `BoundedVec {
-  storage: ${indentBlock(joinArray(values), 1)},
-  len: ${values.length}
+  storage: ${indentBlock(input, 1)},
+  len: ${dataLen}
 }`;
 }
--- ethereum_history_api/oracles/src/script/noir_fixtures/transaction.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/transaction.ts
@@ -5,7 +5,7 @@ import { txTypeToField } from '../../ethereum/receipt.js';
 import { padArray } from '../../util/array.js';
 import { TxRlpEncoder, encodeTx } from '../../ethereum/transaction.js';
 import { ZERO_PAD_VALUE } from '../../noir/oracles/common/const.js';
-import { createBoundedVecFixtureFromArray } from './boundedVec.js';
+import { createBoundedVecFixtureWithLen } from './boundedVec.js';
 import { LEGACY_MAX_TX_RLP_LEN, LEGACY_MAX_TX_ENCODED_LEN } from '../../noir/oracles/common/proofConfig/tx.js';
 import { MAX_DATA_LEN } from '../../noir/oracles/common/txConfig.js';
 
@@ -13,7 +13,6 @@ export function createTransactionFixture(tx: GetTransactionReturnType): string {
   const rlpFields = TxRlpEncoder.txToFields(tx);
   const txRlp = joinArray(padArray(encodeHex(toRlp(rlpFields)), LEGACY_MAX_TX_RLP_LEN, ZERO_PAD_VALUE));
   const encodedTx = joinArray(padArray(encodeHex(encodeTx(tx)), LEGACY_MAX_TX_ENCODED_LEN, ZERO_PAD_VALUE));
-  const input = padArray(encodeHex(tx.input), MAX_DATA_LEN, ZERO_PAD_VALUE, 'left');
 
   const txIdx = encodeField(tx.transactionIndex);
   const to = encodeOptional(tx.to ? joinArray(encodeAddress(tx.to)) : undefined);
@@ -34,7 +33,7 @@ global transaction = TxPartial {
   gas_limit: ${tx.gas},
   to: ${indentBlock(to, 1)},
   value: U128::from_integer(${tx.value}),
-  data: ${indentBlock(createBoundedVecFixtureFromArray(input), 1)},
+  data: ${indentBlock(createBoundedVecFixtureWithLen(tx.input, MAX_DATA_LEN), 1)},
   v: ${v},
   r: ${indentBlock(joinArray(r), 1)},
   s: ${indentBlock(joinArray(s), 1)}
