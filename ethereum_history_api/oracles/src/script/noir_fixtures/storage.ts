8a6309b27e253bdb63734291d78655e22fbe0134
Leonid Logvinov <logvinov.leon@gmail.com>
2024-05-10 09:31:07 +0200

    Storage fixtures (#263)
    * Add storage values to fixtures
    
    * Use map in Serialize of Bytes32 & Address
    
    * Add assertion messages to tests

--- ethereum_history_api/oracles/src/script/noir_fixtures/storage.ts
+++ ethereum_history_api/oracles/src/script/noir_fixtures/storage.ts
@@ -1,11 +1,19 @@
 import { GetProofReturnType } from 'viem';
 import { encodeHexString, joinArray, joinArrayVertical } from '../../noir/noir_js/encode.js';
+import { encodeBytes32 } from '../../noir/oracles/common/encode.js';
 
 export function createStorageFixture(stateProof: GetProofReturnType): string {
+  const values = stateProof.storageProof.map((storageProof) => {
+    return joinArray(encodeBytes32(storageProof.value));
+  });
+
   const keys = stateProof.storageProof.map((storageProof) => {
     return joinArray(encodeHexString(storageProof.key));
   });
+
   const storageFixture = `global keys = ${joinArrayVertical(keys)};
+
+global values = ${joinArrayVertical(values)};
 `;
   return storageFixture;
 }
