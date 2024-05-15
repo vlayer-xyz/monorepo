6628a3a5630fe6166668a0cef9b5698b349f11bd
Leonid Logvinov <logvinov.leon@gmail.com>
2024-05-15 10:08:18 +0200

    Add a script for generating VK

---
+++ ethereum_history_api/oracles/src/script/generateVK.ts
@@ -0,0 +1,8 @@
+import { MonorepoCircuit } from '../noir/circuit/circuit.js';
+import { VerificationKey, generateVk } from '../noir/circuit/vk.js';
+
+const circuit = await MonorepoCircuit.create('../../', 'get_header');
+await generateVk(circuit);
+const vk = await VerificationKey.create(circuit);
+// eslint-disable-next-line no-console
+console.log(vk);
