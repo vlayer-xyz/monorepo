30f91185be70ed3fade41db0edd9073fdb7f777a
Anna Jablonowska <jablonowska999@gmail.com>
2024-05-16 15:15:04 +0200

    Move consts

--- ethereum_history_api/oracles/src/noir/oracles/common/const.ts
+++ ethereum_history_api/oracles/src/noir/oracles/common/const.ts
@@ -1,13 +1,13 @@
 export const MODULUS = 21888242871839275222246405745257275088696311157297823662689037894645226208583n;
+export const BITS = 2n;
+export const BITS_IN_BYTE = 8n;
 export const U1_LEN = 1;
 export const BYTES32_LEN = 32;
 export const ADDRESS_LEN = 20;
 export const U64_LEN = 8;
-export const BITS_IN_BYTE = 8n;
+export const U64_MAX_VALUE = BITS ** (BigInt(U64_LEN) * BITS_IN_BYTE) - 1n;
 export const MAX_U8 = 255;
 export const MAX_TRIE_NODE_LEN = 532;
 export const ZERO_PAD_VALUE = '0x00';
 export const U128_LEN = 16n;
-export const BITS = 2n;
 export const U128_MAX_VALUE = BITS ** (U128_LEN * BITS_IN_BYTE) - 1n;
-export const U64_MAX_VALUE = BITS ** (BigInt(U64_LEN) * BITS_IN_BYTE) - 1n;
