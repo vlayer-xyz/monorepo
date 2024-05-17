34fd09553bc01d2edca6fe286b5f6a05e738e32b
Leonid Logvinov <logvinov.leon@gmail.com>
2024-05-17 15:38:08 +0200

    Simplify VerificationKey class

--- ethereum_history_api/oracles/src/noir/circuit/vk.ts
+++ ethereum_history_api/oracles/src/noir/circuit/vk.ts
@@ -1,21 +1,20 @@
 import { readObject, withTempFile } from '../../util/file.js';
 import { writeFile } from 'fs/promises';
-import { MonorepoCircuit } from './circuit.js';
 import { Barretenberg } from './barretenberg.js';
 
-export async function generateVk(circuit: MonorepoCircuit): Promise<void> {
+export async function generateVk(bytecode: string, vkPath: string, vkAsFieldsPath: string): Promise<void> {
   return await withTempFile(async (acirPath) => {
-    await writeFile(acirPath, Buffer.from(circuit.bytecode(), 'base64'));
+    await writeFile(acirPath, Buffer.from(bytecode, 'base64'));
 
     const barretenberg = await Barretenberg.create();
-    await barretenberg.writeVK(acirPath, circuit.vkPath());
-    await barretenberg.vkAsFields(circuit.vkPath(), circuit.vkAsFieldsPath());
+    await barretenberg.writeVK(acirPath, vkPath);
+    await barretenberg.vkAsFields(vkPath, vkAsFieldsPath);
   });
 }
 
 export class VerificationKey {
-  public static async create(circuit: MonorepoCircuit): Promise<VerificationKey> {
-    const [hash, ...asFields] = await readObject<string[]>(circuit.vkAsFieldsPath());
+  public static async create(path: string): Promise<VerificationKey> {
+    const [hash, ...asFields] = await readObject<string[]>(path);
     return new VerificationKey(hash, asFields);
   }
 
--- ethereum_history_api/oracles/src/script/generateVK.ts
+++ ethereum_history_api/oracles/src/script/generateVK.ts
@@ -2,7 +2,7 @@ import { MonorepoCircuit } from '../noir/circuit/circuit.js';
 import { VerificationKey, generateVk } from '../noir/circuit/vk.js';
 
 const circuit = await MonorepoCircuit.create('../../', 'get_storage');
-await generateVk(circuit);
-const vk = await VerificationKey.create(circuit);
+await generateVk(circuit.artefact.bytecode, circuit.vkPath(), circuit.vkAsFieldsPath());
+const vk = await VerificationKey.create(circuit.vkAsFieldsPath());
 // eslint-disable-next-line no-console
 console.log(vk);
