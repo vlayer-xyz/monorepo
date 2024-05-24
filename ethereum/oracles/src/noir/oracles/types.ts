2ad774bf24e38b28d3627b0ccb9734e5890e0cba
Leonid Logvinov <logvinov.leon@gmail.com>
2024-05-24 12:03:04 +0200

    Add getStorage recursive oracle

--- ethereum/oracles/src/noir/oracles/types.ts
+++ ethereum/oracles/src/noir/oracles/types.ts
@@ -4,4 +4,5 @@ import { MultiChainClient } from '../../ethereum/client.js';
 export type NoirArgument = string[];
 export type NoirArguments = NoirArgument[];
 
-export type Oracle = (multiChainClient: MultiChainClient, args: NoirArguments) => Promise<ForeignCallOutput[]>;
+export type Oracle = (args: NoirArguments) => Promise<ForeignCallOutput[]>;
+export type RpcOracle = (multiChainClient: MultiChainClient, args: NoirArguments) => Promise<ForeignCallOutput[]>;
