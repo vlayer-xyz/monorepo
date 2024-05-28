2f6f99b6768a3b2bc894588806b7e19ad940ae1a
Izabela Ożdżeńska <izabelaozdz@gmail.com>
2024-05-28 14:24:19 +0200

    Remove legacy transaction max values

--- ethereum/oracles/src/noir/oracles/rpc/common/txConfig.ts
+++ ethereum/oracles/src/noir/oracles/rpc/common/txConfig.ts
@@ -1,3 +1,3 @@
-import { MAX_VALUE_LEN } from './proofConfig/tx.js';
+import { MAX_TX_ENCODED_LEN_M } from './proofConfig/tx.js';
 
-export const MAX_DATA_LEN_M = MAX_VALUE_LEN;
+export const MAX_DATA_LEN_M = MAX_TX_ENCODED_LEN_M;
