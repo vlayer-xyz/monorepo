dd5881d020f8ca9d82adf2ec75e040e3cf388c11
Arkadiusz Konior <akonior@gmail.com>
2024-05-29 10:15:49 +0200

    get_log binary

--- ethereum/scripts/e2e_prep.sh
+++ ethereum/scripts/e2e_prep.sh
@@ -3,6 +3,7 @@ nargo compile --package get_account
 nargo compile --package get_storage
 nargo compile --package get_receipt
 nargo compile --package get_transaction
+nargo compile --package get_log
 
 echo "Prove get_header"
 nargo prove --package get_header --oracle-resolver=http://localhost:5555
@@ -14,6 +15,8 @@ echo "Prove get_receipt"
 nargo prove --package get_receipt --oracle-resolver=http://localhost:5555
 echo "Prove get_transaction"
 nargo prove --package get_transaction --oracle-resolver=http://localhost:5555
+echo "Prove get_log"
+nargo prove --package get_log --oracle-resolver=http://localhost:5555
 
 echo "Codegen get_header"
 nargo codegen-verifier --package get_header
@@ -25,3 +28,5 @@ echo "Codegen get_receipt"
 nargo codegen-verifier --package get_receipt
 echo "Codegen get_transaction"
 nargo codegen-verifier --package get_transaction
+echo "Codegen get_log"
+nargo codegen-verifier --package get_log
