ee46af88c025863872234eb05d890e1e447907cb
Arkadiusz Konior <akonior@gmail.com>
2024-06-07 11:00:42 +0200

    is_dao_worthy_recursive

--- ethereum/oracles/src/script/generateGetStorageVK.ts
+++ ethereum/oracles/src/script/generateGetStorageVK.ts
@@ -1,7 +1,7 @@
 import { MonorepoCircuit } from '../noir/circuit/circuit.js';
 import { VerificationKey, generateVk } from '../noir/circuit/vk.js';
 
-const circuit = await MonorepoCircuit.create('../../', 'get_storage');
+const circuit = await MonorepoCircuit.create('../../', 'get_storage_recursive');
 await generateVk(circuit.artefact.bytecode, circuit.vkPath(), circuit.vkAsFieldsPath());
 const vk = await VerificationKey.create(circuit.vkAsFieldsPath());
 // eslint-disable-next-line no-console
